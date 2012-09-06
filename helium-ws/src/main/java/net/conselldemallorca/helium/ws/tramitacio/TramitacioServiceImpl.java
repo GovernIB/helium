e8202d584133edb66356863a3bfbd59064ea7e33
josepg-limit <josepg-limit@e3271b2b-fd41-4d56-bedf-4b5636213925>
2012-09-06 06:41:13 +0000

    mid: 0000304 msg: Error al cridar al WS De tramitació
    git-svn-id: svn://svn.code.sf.net/p/gehelium/code/helium/branches/helium-2.4@476 e3271b2b-fd41-4d56-bedf-4b5636213925

--- helium-ws/src/main/java/net/conselldemallorca/helium/ws/tramitacio/TramitacioServiceImpl.java
+++ helium-ws/src/main/java/net/conselldemallorca/helium/ws/tramitacio/TramitacioServiceImpl.java
@@ -43,6 +43,7 @@ import org.springframework.beans.factory.annotation.Autowired;
  * @author Limit Tecnologies <limit@limit.es>
  */
 @WebService(
+		serviceName="TramitacioService",
 		endpointInterface = "net.conselldemallorca.helium.ws.tramitacio.TramitacioService",
 		targetNamespace = "http://tramitacio.integracio.helium.conselldemallorca.net/")
 public class TramitacioServiceImpl implements TramitacioService {
