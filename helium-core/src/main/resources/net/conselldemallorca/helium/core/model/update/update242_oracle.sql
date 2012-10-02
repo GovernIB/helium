850d1ec35fbb8eaad8bce85d22339ca63227c994
sandreu-limit <sandreu-limit@e3271b2b-fd41-4d56-bedf-4b5636213925>
2012-10-02 07:35:39 +0000

    MID:	0000314 MSG:	Poder marcar variables que no es puguin retrocedir
    git-svn-id: svn://svn.code.sf.net/p/gehelium/code/helium/branches/helium-2.4@517 e3271b2b-fd41-4d56-bedf-4b5636213925

--- helium-core/src/main/resources/net/conselldemallorca/helium/core/model/update/update242_oracle.sql
+++ helium-core/src/main/resources/net/conselldemallorca/helium/core/model/update/update242_oracle.sql
@@ -4,4 +4,8 @@ ALTER TABLE HEL_PORTASIGNATURES ADD DATA_CB_PRI TIMESTAMP(6);
 ALTER TABLE HEL_PORTASIGNATURES ADD ERROR_CB_PROCES CLOB;
 
 --nou camp check per evitar esborrar el contingut de la variable en cas de retrocedir expedient
-ALTER TABLE HEL_CAMP add ignored DEFAULT 0;
+ALTER TABLE HEL_CAMP add ignored number(1) DEFAULT 0;
+
+
+--assignació de tasques per tipus d'expedient
+ALTER TABLE HEL_REDIR ADD tipusExpedientId NUMBER(19);
