8bf22a718cc7632ee2adebcab88aa15141d28cba
sandreu-limit <sandreu-limit@e3271b2b-fd41-4d56-bedf-4b5636213925>
2012-10-10 10:51:41 +0000

    MID:	0000318 MSG:	Ocultar expedients a les consultes per tipus [helium-2.4] Nova funcionalitat
    git-svn-id: svn://svn.code.sf.net/p/gehelium/code/helium/branches/helium-2.4@534 e3271b2b-fd41-4d56-bedf-4b5636213925

--- helium-core/src/main/resources/net/conselldemallorca/helium/core/model/update/update243_oracle.sql
+++ helium-core/src/main/resources/net/conselldemallorca/helium/core/model/update/update243_oracle.sql
@@ -1,2 +1,2 @@
 --nou camp check per ocultar expedients a les consultes per tipus
-alter table hel_consulta add column ocultar_actiu BOOLEAN SET DEFAULT 0;
+ALTER TABLE HEL_CONSULTA ADD (OCULTAR_ACTIU NUMBER(1) DEFAULT 0 NOT NULL);
