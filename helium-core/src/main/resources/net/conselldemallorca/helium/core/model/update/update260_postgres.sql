e4dcf2eb1a2d256aae77f27376f44ea9cd2b6cb1
sandreu-limit <sandreu-limit@e3271b2b-fd41-4d56-bedf-4b5636213925>
2013-05-02 07:26:15 +0000

    MID: 448 MSG: Execució massiva: scripts de base de dades
    git-svn-id: svn://svn.code.sf.net/p/gehelium/code/helium/branches/helium-2.6@774 e3271b2b-fd41-4d56-bedf-4b5636213925

---
+++ helium-core/src/main/resources/net/conselldemallorca/helium/core/model/update/update260_postgres.sql
@@ -0,0 +1,48 @@
+-- Taules per a execució massiva
+CREATE TABLE hel_exec_massiva (
+	id BIGINT NOT NULL,
+	data_fi TIMESTAMP(6) WITHOUT TIME ZONE,
+	data_inici TIMESTAMP(6) WITHOUT TIME ZONE NOT NULL,
+	env_correu BOOLEAN,
+	param1 CHARACTER VARYING(255),
+	param2 OID,
+	tipus INTEGER NOT NULL,
+	usuari CHARACTER VARYING(64) NOT NULL,
+	expedient_tipus_id BIGINT NOT NULL,
+	PRIMARY KEY (id),
+	CONSTRAINT hel_exptipus_exemas_fk FOREIGN KEY (expedient_tipus_id) REFERENCES public.hel_expedient_tipus (id)
+);
+    
+CREATE TABLE hel_exec_masexp (
+	id BIGINT NOT NULL,
+	data_fi TIMESTAMP(6) WITHOUT TIME ZONE,
+	data_inici TIMESTAMP(6) WITHOUT TIME ZONE,
+	estat INTEGER NOT NULL,
+	ordre INTEGER NOT NULL,
+	execmas_id BIGINT NOT NULL,
+	expedient_id BIGINT NOT NULL,
+	error TEXT,
+	PRIMARY KEY (id),
+	CONSTRAINT hel_execmas_exemasex_fk FOREIGN KEY (execmas_id) REFERENCES public.hel_exec_massiva (id),
+	CONSTRAINT hel_expedient_exemasex_fk FOREIGN KEY (expedient_id) REFERENCES public.hel_expedient (id)
+);
+
+
+-- Actualització a la nova versió --
+insert into hel_versio (
+    id,
+    codi,
+    ordre,
+    data_creacio,
+    proces_executat,
+    script_executat,
+    data_execucio_script)
+select
+    nextval('hibernate_sequence') id,
+    '2.6.0' codi,
+    260 ordre,
+    'now' data_creacio,
+    false proces_executat,
+    true script_executat,
+    'now' data_execucio_script
+where (select count(*) from hel_versio where ordre = 260) = 0;
