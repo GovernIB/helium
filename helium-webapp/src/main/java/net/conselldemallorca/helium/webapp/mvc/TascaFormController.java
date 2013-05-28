c8a20f3eaddb9d7dc18c14fb1188e9c18bede13e
josepg-limit <josepg-limit@e3271b2b-fd41-4d56-bedf-4b5636213925>
2013-05-28 11:52:55 +0000

    mid: 0000486 msg: Problema fianlització tasques sense pitjar botó de validar
    git-svn-id: svn://svn.code.sf.net/p/gehelium/code/helium/branches/helium-2.6@810 e3271b2b-fd41-4d56-bedf-4b5636213925

--- helium-webapp/src/main/java/net/conselldemallorca/helium/webapp/mvc/TascaFormController.java
+++ helium-webapp/src/main/java/net/conselldemallorca/helium/webapp/mvc/TascaFormController.java
@@ -316,7 +316,7 @@ public class TascaFormController extends BaseController {
 				}
 				status.setComplete();
 				if (finalitzarAmbOutcome != null && !finalitzarAmbOutcome.equals("@#@")) {
-					return "redirect:/tasca/completar.html?id=" + id + "&pipella=form&submit=" + submit;
+					return "redirect:/tasca/completar.html?id=" + id + "&pipella=form&submit=" + finalitzarAmbOutcome;
 				} else {
 		        	if (iframe != null)
 		        		return "redirect:/tasca/formIframe.html?id=" + id + "&iframe=iframe";
