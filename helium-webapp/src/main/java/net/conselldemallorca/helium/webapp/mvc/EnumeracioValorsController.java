49767156e24e2853e40f8ec3782ee8ed80d43fbb
josepg-limit <josepg-limit@e3271b2b-fd41-4d56-bedf-4b5636213925>
2013-06-04 14:03:03 +0000

    mid: 0000497 msg: S'afegeixen caràcters raros a les importacions de dades de enumeracions
    git-svn-id: svn://svn.code.sf.net/p/gehelium/code/helium/branches/helium-2.6@821 e3271b2b-fd41-4d56-bedf-4b5636213925

--- helium-webapp/src/main/java/net/conselldemallorca/helium/webapp/mvc/EnumeracioValorsController.java
+++ helium-webapp/src/main/java/net/conselldemallorca/helium/webapp/mvc/EnumeracioValorsController.java
@@ -261,7 +261,12 @@ public class EnumeracioValorsController extends BaseController {
 						if (columnes.length > 1) {
 							EnumeracioValors enumeracioValors = new EnumeracioValors();
 				        	enumeracioValors.setId(null);
-				        	enumeracioValors.setCodi(columnes[0]);
+				        	// Per evitar caràcters estranys al codi de l'enumeració
+				        	String codi = columnes[0];
+				        	while (!codi.matches("^\\w.*")) {
+				        		codi = codi.substring(1);
+				        	}
+				        	enumeracioValors.setCodi(codi);
 				        	enumeracioValors.setNom(columnes[1]);
 				        	enumeracioValors.setEnumeracio(enumeracio);
 			        		dissenyService.createEnumeracioValors(enumeracioValors);
