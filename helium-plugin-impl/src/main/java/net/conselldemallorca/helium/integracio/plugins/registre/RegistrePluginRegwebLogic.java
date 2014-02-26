3b2afb950db045b89cd355fd4e4c46af800ac0d7
josepg-limit <josepg-limit@e3271b2b-fd41-4d56-bedf-4b5636213925>
2014-02-26 14:02:08 +0000

    mid: 613 msg: Error al registrar des de les accions massives
    git-svn-id: svn://svn.code.sf.net/p/gehelium/code/helium/branches/helium-2.6@1055 e3271b2b-fd41-4d56-bedf-4b5636213925

--- helium-plugin-impl/src/main/java/net/conselldemallorca/helium/integracio/plugins/registre/RegistrePluginRegwebLogic.java
+++ helium-plugin-impl/src/main/java/net/conselldemallorca/helium/integracio/plugins/registre/RegistrePluginRegwebLogic.java
@@ -43,7 +43,6 @@ public class RegistrePluginRegwebLogic implements RegistrePlugin {
 		ejbUtil = new EjbUtil();
 	}
 
-	@SuppressWarnings("unchecked")
 	public RespostaAnotacioRegistre registrarEntrada(
 			RegistreEntrada registreEntrada) throws RegistrePluginException {
 		try {
@@ -117,31 +116,18 @@ public class RegistrePluginRegwebLogic implements RegistrePlugin {
 			} else {
 				throw new RegistrePluginException("S'ha d'especificar algun document per registrar");
 			}
-			RegistroEntradaFacade registroEntrada = getRegistreEntradaService();
-			ParametrosRegistroEntrada respostaValidacio = registroEntrada.validar(params);
-			if (respostaValidacio.getValidado()) {
-				ParametrosRegistroEntrada respostaGrabacio = registroEntrada.grabar(params);
-				RespostaAnotacioRegistre resposta = new RespostaAnotacioRegistre();
-				if (respostaGrabacio.getGrabado()) {
-					resposta.setErrorCodi(RespostaAnotacioRegistre.ERROR_CODI_OK);
-					resposta.setNumero(
-							respostaGrabacio.getNumeroEntrada() +
-							SEPARADOR_NUMERO +
-							respostaGrabacio.getAnoEntrada());
-					resposta.setData(ara);
-					return resposta;
-				} else {
-					throw new RegistrePluginException("No s'ha pogut guardar l'entrada");
-				}
+			ExecValidarGrabarEntradaThread execThread = new ExecValidarGrabarEntradaThread(
+					ara,
+					params);
+			execThread.start();
+			execThread.join();
+			if (execThread.getException() != null) {
+				throw execThread.getException();
 			} else {
-				StringBuilder sb = new StringBuilder();
-				sb.append("Errors de validació:\n");
-				Map<String, String> errors = respostaValidacio.getErrores();
-				for (String camp: errors.keySet()) {
-					sb.append(" | " + errors.get(camp));
-				}
-				throw new RegistrePluginException("S'han produit errors de validació de l'entrada: " + sb.toString());
+				return execThread.getReturned();
 			}
+		} catch (RegistrePluginException rpex) {
+			throw rpex;
 		} catch (Exception ex) {
 			logger.error("Error al registrar l'entrada", ex);
 			throw new RegistrePluginException("Error al registrar l'entrada", ex);
@@ -161,8 +147,16 @@ public class RegistrePluginRegwebLogic implements RegistrePlugin {
 				throw new RegistrePluginException("El número de registre a consultar (" + registreNumero + ") no té el format correcte");
 			params.setNumeroEntrada(registreNumero.substring(0, index));
 			params.setAnoEntrada(registreNumero.substring(index + 1));
-			RegistroEntradaFacade registroEntrada = getRegistreEntradaService();
-			ParametrosRegistroEntrada llegit = registroEntrada.leer(params);
+			ExecLeerEntradaThread execThread = new ExecLeerEntradaThread(
+					params);
+			execThread.start();
+			execThread.join();
+			ParametrosRegistroEntrada llegit = null;
+			if (execThread.getException() != null) {
+				throw execThread.getException();
+			} else {
+				llegit = execThread.getReturned();
+			}
 			RespostaConsulta resposta = new RespostaConsulta();
 			resposta.setRegistreNumero(registreNumero);
 			resposta.setRegistreData(new SimpleDateFormat("dd/MM/yyyy HH:mm").parse(llegit.getDataEntrada() + " " + llegit.getHora()));
@@ -191,13 +185,14 @@ public class RegistrePluginRegwebLogic implements RegistrePlugin {
 			documents.add(document);
 			resposta.setDocuments(documents);
 			return resposta;
+		} catch (RegistrePluginException rpex) {
+			throw rpex;
 		} catch (Exception ex) {
 			logger.error("Error al consultar l'entrada", ex);
 			throw new RegistrePluginException("Error al consultar l'entrada", ex);
 		}
 	}
 
-	@SuppressWarnings("unchecked")
 	public RespostaAnotacioRegistre registrarSortida(
 			RegistreSortida registreSortida) throws RegistrePluginException {
 		try {
@@ -270,31 +265,18 @@ public class RegistrePluginRegwebLogic implements RegistrePlugin {
 			} else {
 				throw new RegistrePluginException("S'ha d'especificar algun document per registrar");
 			}
-			RegistroSalidaFacade registroSalida = getRegistreSortidaService();
-			ParametrosRegistroSalida respostaValidacio = registroSalida.validar(params);
-			if (respostaValidacio.getValidado()) {
-				ParametrosRegistroSalida respostaGrabacio = registroSalida.grabar(params);
-				RespostaAnotacioRegistre resposta = new RespostaAnotacioRegistre();
-				if (respostaGrabacio.getGrabado()) {
-					resposta.setErrorCodi(RespostaAnotacioRegistre.ERROR_CODI_OK);
-					resposta.setNumero(
-							respostaGrabacio.getNumeroSalida() +
-							SEPARADOR_NUMERO +
-							respostaGrabacio.getAnoSalida());
-					resposta.setData(ara);
-					return resposta;
-				} else {
-					throw new RegistrePluginException("No s'ha pogut guardar la sortida");
-				}
+			ExecValidarGrabarSortidaThread execThread = new ExecValidarGrabarSortidaThread(
+					ara,
+					params);
+			execThread.start();
+			execThread.join();
+			if (execThread.getException() != null) {
+				throw execThread.getException();
 			} else {
-				StringBuilder sb = new StringBuilder();
-				sb.append("Errors de validació:\n");
-				Map<String, String> errors = respostaValidacio.getErrores();
-				for (String camp: errors.keySet()) {
-					sb.append(" | " + errors.get(camp));
-				}
-				throw new RegistrePluginException("S'han produit errors de validació de l'entrada: " + sb.toString());
+				return execThread.getReturned();
 			}
+		} catch (RegistrePluginException rpex) {
+			throw rpex;
 		} catch (Exception ex) {
 			logger.error("Error al registrar la sortida", ex);
 			throw new RegistrePluginException("Error al registrar la sortida", ex);
@@ -315,8 +297,16 @@ public class RegistrePluginRegwebLogic implements RegistrePlugin {
 				throw new RegistrePluginException("El número de registre a consultar (" + registreNumero + ") no té el format correcte");
 			params.setNumeroSalida(registreNumero.substring(0, index));
 			params.setAnoSalida(registreNumero.substring(index + 1));
-			RegistroSalidaFacade registroSalida = getRegistreSortidaService();
-			ParametrosRegistroSalida llegit = registroSalida.leer(params);
+			ExecLeerSalidaThread execThread = new ExecLeerSalidaThread(
+					params);
+			execThread.start();
+			execThread.join();
+			ParametrosRegistroSalida llegit = null;
+			if (execThread.getException() != null) {
+				throw execThread.getException();
+			} else {
+				llegit = execThread.getReturned();
+			}
 			RespostaConsulta resposta = new RespostaConsulta();
 			resposta.setRegistreNumero(registreNumero);
 			resposta.setRegistreData(new SimpleDateFormat("dd/MM/yyyy HH:mm").parse(llegit.getDataSalida() + " " + llegit.getHora()));
@@ -346,6 +336,8 @@ public class RegistrePluginRegwebLogic implements RegistrePlugin {
 			documents.add(document);
 			resposta.setDocuments(documents);
 			return resposta;
+		} catch (RegistrePluginException rpex) {
+			throw rpex;
 		} catch (Exception ex) {
 			logger.error("Error al consultar la sortida", ex);
 			throw new RegistrePluginException("Error al consultar la sortida", ex);
@@ -364,10 +356,19 @@ public class RegistrePluginRegwebLogic implements RegistrePlugin {
 	@SuppressWarnings({"rawtypes", "unused"})
 	public String obtenirNomOficina(String oficinaCodi) throws RegistrePluginException {
 		try {
+			String nomObtingut = null;
 			if (oficinaCodi != null) {
 				int indexBarra = oficinaCodi.indexOf(SEPARADOR_ENTITAT);
 				if (indexBarra != -1) {
-					Vector v = getValoresService().buscarOficinasFisicasDescripcion("tots", "totes");
+					ExecBuscarOficinas execThread = new ExecBuscarOficinas();
+					execThread.start();
+					execThread.join();
+					Vector v = null;
+					if (execThread.getException() != null) {
+						throw execThread.getException();
+					} else {
+						v = execThread.getReturned();
+					}
 					Iterator it = v.iterator();
 					while (it.hasNext()) {
 						String codiOficina = (String)it.next();
@@ -375,12 +376,16 @@ public class RegistrePluginRegwebLogic implements RegistrePlugin {
 						String nomOficinaFisica = (String)it.next();
 						String nomOficina = (String)it.next();
 						String textComparacio = codiOficina + SEPARADOR_ENTITAT + codiOficinaFisica;
-						if (textComparacio.equals(oficinaCodi))
-							return nomOficina;
+						if (textComparacio.equals(oficinaCodi)) {
+							nomObtingut = nomOficina;
+							break;
+						}
 					}
 				}
 			}
-			return null;
+			return nomObtingut;
+		} catch (RegistrePluginException rpex) {
+			throw rpex;
 		} catch (Exception ex) {
 			logger.error("Error al obtenir el nom de l'oficina " + oficinaCodi, ex);
 			throw new RegistrePluginException("Error al obtenir el nom de l'oficina " + oficinaCodi, ex);
@@ -457,6 +462,174 @@ public class RegistrePluginRegwebLogic implements RegistrePlugin {
 			return "F";
 		}
 		return "2";
+	}
+
+	private class ExecValidarGrabarEntradaThread extends Thread {
+		private Date ara;
+		private ParametrosRegistroEntrada params;
+		private RespostaAnotacioRegistre returned;
+		private Exception exception;
+		public ExecValidarGrabarEntradaThread(
+				Date ara,
+				ParametrosRegistroEntrada params) {
+			this.ara = ara;
+			this.params = params;
+		}
+		public void run() {
+			try {
+				RegistroEntradaFacade registroEntrada = getRegistreEntradaService();
+				ParametrosRegistroEntrada respostaValidacio = registroEntrada.validar(params);
+				if (respostaValidacio.getValidado()) {
+					ParametrosRegistroEntrada respostaGrabacio = registroEntrada.grabar(params);
+					RespostaAnotacioRegistre resposta = new RespostaAnotacioRegistre();
+					if (respostaGrabacio.getGrabado()) {
+						resposta.setErrorCodi(RespostaAnotacioRegistre.ERROR_CODI_OK);
+						resposta.setNumero(
+								respostaGrabacio.getNumeroEntrada() +
+								SEPARADOR_NUMERO +
+								respostaGrabacio.getAnoEntrada());
+						resposta.setData(ara);
+						returned = resposta;
+					} else {
+						throw new RegistrePluginException("No s'ha pogut guardar l'entrada");
+					}
+				} else {
+					StringBuilder sb = new StringBuilder();
+					sb.append("Errors de validació:\n");
+					@SuppressWarnings("unchecked")
+					Map<String, String> errors = respostaValidacio.getErrores();
+					for (String camp: errors.keySet()) {
+						sb.append(" | " + errors.get(camp));
+					}
+					exception = new RegistrePluginException("S'han produit errors de validació de l'entrada: " + sb.toString());
+				}
+			} catch (Exception ex) {
+				exception = ex;
+			}
+		}
+		public RespostaAnotacioRegistre getReturned() {
+			return returned;
+		}
+		public Exception getException() {
+			return exception;
+		}
+	}
+	private class ExecLeerEntradaThread extends Thread {
+		private ParametrosRegistroEntrada params;
+		private ParametrosRegistroEntrada returned;
+		private Exception exception;
+		public ExecLeerEntradaThread(ParametrosRegistroEntrada params) {
+			this.params = params;
+		}
+		public void run() {
+			try {
+				RegistroEntradaFacade registroEntrada = getRegistreEntradaService();
+				returned = registroEntrada.leer(params);
+			} catch (Exception ex) {
+				exception = ex;
+			}
+		}
+		public ParametrosRegistroEntrada getReturned() {
+			return returned;
+		}
+		public Exception getException() {
+			return exception;
+		}
+	}
+	private class ExecValidarGrabarSortidaThread extends Thread {
+		private Date ara;
+		private ParametrosRegistroSalida params;
+		private RespostaAnotacioRegistre returned;
+		private Exception exception;
+		public ExecValidarGrabarSortidaThread(
+				Date ara,
+				ParametrosRegistroSalida params) {
+			this.ara = ara;
+			this.params = params;
+		}
+		public void run() {
+			try {
+				RegistroSalidaFacade registroSalida = getRegistreSortidaService();
+				ParametrosRegistroSalida respostaValidacio = registroSalida.validar(params);
+				if (respostaValidacio.getValidado()) {
+					ParametrosRegistroSalida respostaGrabacio = registroSalida.grabar(params);
+					RespostaAnotacioRegistre resposta = new RespostaAnotacioRegistre();
+					if (respostaGrabacio.getGrabado()) {
+						resposta.setErrorCodi(RespostaAnotacioRegistre.ERROR_CODI_OK);
+						resposta.setNumero(
+								respostaGrabacio.getNumeroSalida() +
+								SEPARADOR_NUMERO +
+								respostaGrabacio.getAnoSalida());
+						resposta.setData(ara);
+						returned = resposta;
+					} else {
+						throw new RegistrePluginException("No s'ha pogut guardar la sortida");
+					}
+				} else {
+					StringBuilder sb = new StringBuilder();
+					sb.append("Errors de validació:\n");
+					@SuppressWarnings("unchecked")
+					Map<String, String> errors = respostaValidacio.getErrores();
+					for (String camp: errors.keySet()) {
+						sb.append(" | " + errors.get(camp));
+					}
+					throw new RegistrePluginException("S'han produit errors de validació de l'entrada: " + sb.toString());
+				}
+			} catch (Exception ex) {
+				exception = ex;
+			}
+		}
+		public RespostaAnotacioRegistre getReturned() {
+			return returned;
+		}
+		public Exception getException() {
+			return exception;
+		}
+	}
+	private class ExecLeerSalidaThread extends Thread {
+		private ParametrosRegistroSalida params;
+		private ParametrosRegistroSalida returned;
+		private Exception exception;
+		public ExecLeerSalidaThread(ParametrosRegistroSalida params) {
+			this.params = params;
+		}
+		public void run() {
+			try {
+				RegistroSalidaFacade registroSalida = getRegistreSortidaService();
+				returned = registroSalida.leer(params);
+			} catch (Exception ex) {
+				exception = ex;
+			}
+		}
+		public ParametrosRegistroSalida getReturned() {
+			return returned;
+		}
+		public Exception getException() {
+			return exception;
+		}
+	}
+	private class ExecBuscarOficinas extends Thread {
+		@SuppressWarnings("rawtypes")
+		private Vector returned;
+		private Exception exception;
+		public ExecBuscarOficinas() {
+		}
+		public void run() {
+			try {
+				returned = getValoresService().buscarOficinasFisicasDescripcion(
+						"tots",
+						"totes");
+			} catch (Exception ex) {
+				exception = ex;
+			}
+		}
+		@SuppressWarnings("rawtypes")
+		public Vector getReturned() {
+			return returned;
+		}
+		public Exception getException() {
+			return exception;
+		}
 	}
 
 	private static final Log logger = LogFactory.getLog(RegistrePluginRegwebLogic.class);
