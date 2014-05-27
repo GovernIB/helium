416a8085aa7dab1aaf3aa8d496829f4a193fa924
sandreu-limit <sandreu-limit@e3271b2b-fd41-4d56-bedf-4b5636213925>
2014-05-27 13:19:31 +0000

    MID: 679. MSG: Error al generar documentos en una tarea
    git-svn-id: svn://svn.code.sf.net/p/gehelium/code/helium/branches/helium-3.0@1212 e3271b2b-fd41-4d56-bedf-4b5636213925

--- helium-core-v26/src/main/java/net/conselldemallorca/helium/core/model/hibernate/DocumentTasca.java
+++ helium-core-v26/src/main/java/net/conselldemallorca/helium/core/model/hibernate/DocumentTasca.java
@@ -100,7 +100,7 @@ public class DocumentTasca implements Serializable, GenericEntity<Long> {
 		this.order = order;
 	}
 
-	@ManyToOne(optional=false, fetch=FetchType.LAZY)
+	@ManyToOne(optional=false)
 	@JoinColumn(name="document_id")
 	@ForeignKey(name="hel_document_doctasca_fk")
 	public Document getDocument() {
