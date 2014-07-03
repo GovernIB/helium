8eaf70b591b76442e79f56c82dd2f69db9e7e869
sandreu-limit <sandreu-limit@e3271b2b-fd41-4d56-bedf-4b5636213925>
2014-07-03 11:30:49 +0000

    MID: 655 MSG: Jocs de proves amb Selelium v.3
    git-svn-id: svn://svn.code.sf.net/p/gehelium/code/helium/branches/helium-3.0@1275 e3271b2b-fd41-4d56-bedf-4b5636213925

--- helium-selenium/src/test/java/net/conselldemallorca/helium/test/consulta/Expedient.java
+++ helium-selenium/src/test/java/net/conselldemallorca/helium/test/consulta/Expedient.java
@@ -3,7 +3,11 @@ package net.conselldemallorca.helium.test.consulta;
 import static org.junit.Assert.assertTrue;
 
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
@@ -13,6 +17,7 @@ import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class Expedient extends BaseTest {	
@@ -25,7 +30,232 @@ public class Expedient extends BaseTest {
 	static String entornActual;
 
 	@Test
-	public void h_acces_a_tramitacio_de_tasca() throws InterruptedException, ParseException {
+	public void c_netejar_filtre() throws InterruptedException, ParseException {	
+		carregarUrlConfiguracio();
+		
+		seleccionarEntorno(entorn);
+		
+		actions.moveToElement(driver.findElement(By.id("menuConsultes")));
+		actions.build().perform();
+		actions.moveToElement(driver.findElement(By.xpath("//*[@id='menuConsultes']/ul/li[1]/a")));
+		actions.click();
+		actions.build().perform();
+		
+		// Rellenamos los datos
+		for (WebElement input : driver.findElements(By.xpath("//*[@id='command']/div/div/input"))) {
+			input.sendKeys(input.getAttribute("name"));
+		}
+		for (WebElement select : driver.findElements(By.xpath("//*[@id='command']/div/div/select"))) {
+			List<WebElement> options = select.findElements(By.tagName("option"));
+			options.get(options.size()-1).click();
+		}		
+		Calendar calendar = Calendar.getInstance();
+		for (WebElement fecha : driver.findElements(By.xpath("//*[@id='command']/div/div/div/label/input"))) {
+			calendar.add(Calendar.DATE, 1);
+			fecha.sendKeys(new SimpleDateFormat("dd/MM/yyyy").format(calendar.getTime()));
+		}		
+		driver.findElement(By.xpath("//*[@id='command']/div[2]/div[6]/button[1]")).click();
+		
+		// Limpiamos los datos
+		driver.findElement(By.xpath("//button[contains(text(), 'Netejar')]")).click();
+		
+		// Comprobamos que están vacíos
+		for (WebElement input : driver.findElements(By.xpath("//*[@id='command']/div/div/input"))) {
+			assertTrue("El campo '"+input.getAttribute("name")+"' no se limpió", input.getAttribute("value").isEmpty());
+		}
+		for (WebElement select : driver.findElements(By.xpath("//*[@id='command']/div/div/select"))) {
+			List<WebElement> options = select.findElements(By.tagName("option"));
+			int i = 0;
+			for (WebElement option : options) {
+				if (option.isSelected()) {
+					break;
+				}
+				i++;
+			}
+			assertTrue("El campo '"+select.getAttribute("name")+"' no se limpió", i == 0);
+		}		
+		for (WebElement fecha : driver.findElements(By.xpath("//*[@id='command']/div/div/div/label/input"))) {
+			assertTrue("El campo '"+fecha.getAttribute("name")+"' no se limpió", fecha.getAttribute("value").isEmpty());
+		}		
+		driver.findElement(By.xpath("//*[@id='command']/div[2]/div[6]/button[1]")).click();
+		
+	}
+	
+	@Test
+	public void d_anular_expedient() throws InterruptedException, ParseException {	
+		carregarUrlConfiguracio();
+		
+		seleccionarEntorno(entorn);
+		
+		// Comprobamos permisos de borrado
+		
+		// Los quitamos
+		assignarPermisosTipusExpedient(codTipusExp, usuari, "DESIGN","CREATE","SUPERVISION","DELETE","MANAGE","READ");
+		
+		consultarExpedientes(null, null, tipusExp);
+		
+		noExisteixElementAssert("//*[@id='registre']/tbody/tr[1]/td[contains(a/img/@src,'/helium/img/delete.png')]/a/img", "Tenía permisos de anulado");
+		
+		// Los ponemos
+		assignarPermisosTipusExpedient(codTipusExp, usuari, "DESIGN","CREATE","SUPERVISION","WRITE","MANAGE","DELETE","READ","ADMINISTRATION");
+		
+		consultarExpedientes(null, null, tipusExp);
+		
+		existeixElementAssert("//*[@id='registre']/tbody/tr[1]/td[contains(a/img/@src,'/helium/img/delete.png')]/a/img", "No tenía permisos de anulado");
+		
+		// Anulamos el primer expediente
+		driver.findElement(By.xpath("//*[@id='registre']/tbody/tr[1]/td[7]/a")).click();
+		acceptarConfirm("El motivo");
+	}
+
+	@Test
+	public void e_mostrar_anulats() throws InterruptedException, ParseException {
+		carregarUrlConfiguracio();
+		
+		seleccionarEntorno(entorn);
+		
+		actions.moveToElement(driver.findElement(By.id("menuConsultes")));
+		actions.build().perform();
+		actions.moveToElement(driver.findElement(By.xpath("//*[@id='menuConsultes']/ul/li[1]/a")));
+		actions.click();
+		actions.build().perform();
+		
+		WebElement selectTipusExpedient = driver.findElement(By.xpath("//*[@id='expedientTipus0']"));
+		List<WebElement> options = selectTipusExpedient.findElements(By.tagName("option"));
+		for (WebElement option : options) {
+			if (option.getText().equals(tipusExp)) {
+				option.click();
+				break;
+			}
+		}
+		
+		// Seleccionamos los activos y anulados
+		WebElement select = driver.findElement(By.xpath("//*[@id='mostrarAnulats0']"));
+		options = select.findElements(By.tagName("option"));
+		options.get(2).click();	
+		
+		driver.findElement(By.xpath("//*[@id='command']/div[2]/div[6]/button[1]")).click();	
+		
+		// Seleccionamos todos
+		select = driver.findElement(By.xpath("//*[@id='objectsPerPage']"));
+		options = select.findElements(By.tagName("option"));
+		options.get(options.size()-1).click();
+		
+		driver.findElement(By.xpath("//*[@id='command']/div[2]/div[6]/button[1]")).click();	
+		
+		// Contamos los anulados
+		List<String> idsExpedient = new ArrayList<String>();
+		
+		for (WebElement input : driver.findElements(By.xpath("//img[@src='/helium/img/arrow_undo.png']/parent::a/parent::td/parent::tr/td[1]/input[@type='checkbox']"))) {
+			idsExpedient.add(input.getAttribute("value"));
+		}
+		
+		if (!idsExpedient.isEmpty()) {
+			// Seleccionamos los anulados
+			select = driver.findElement(By.xpath("//*[@id='mostrarAnulats0']"));
+			options = select.findElements(By.tagName("option"));
+			options.get(1).click();
+			
+			driver.findElement(By.xpath("//*[@id='command']/div[2]/div[6]/button[1]")).click();	
+			
+			// Número de expedientes
+			String numExpedientes = driver.findElement(By.xpath("//*[@id='content']/span[1]")).getText();
+			numExpedientes = numExpedientes.substring("S'han trobat ".length(), numExpedientes.indexOf(" items,"));
+			
+			assertTrue("El número de expedientes anulados no coincidía", idsExpedient.size() == Integer.valueOf(numExpedientes));
+			
+			for (WebElement input : driver.findElements(By.xpath("//img[@src='/helium/img/arrow_undo.png']/parent::a/parent::td/parent::tr/td[1]/input[@type='checkbox']"))) {
+				String idExpedient = input.getAttribute("value");
+				assertTrue("El expediente con ID '"+idExpedient+"' no se encontró en la anterior pantalla de activos y anulados", idsExpedient.contains(idExpedient));
+			}
+		}
+	}
+
+	@Test
+	public void m_filtrar_nomes_amb_tasques_pendents() throws InterruptedException, ParseException {
+	}
+
+	@Test
+	public void g_resultats_per_pagina() throws InterruptedException, ParseException {
+		carregarUrlConfiguracio();
+		
+		seleccionarEntorno(entorn);
+		
+		consultarExpedientes(null, null, tipusExp);
+		
+		// Número de expedientes
+		String numExpedientes = driver.findElement(By.xpath("//*[@id='content']/span[1]")).getText();
+		numExpedientes = numExpedientes.substring("S'han trobat ".length(), numExpedientes.indexOf(" items,"));
+						
+		// Seleccionamos página por página
+		int i = 0;
+		int numPages = driver.findElement(By.xpath("//*[@id='objectsPerPage']")).findElements(By.tagName("option")).size()-1;
+		while (i < numPages) {
+			driver.findElement(By.xpath("//*[@id='objectsPerPage']")).findElements(By.tagName("option")).get(i).click();
+			
+			driver.findElement(By.xpath("//*[@id='command']/div[2]/div[6]/button[1]")).click();	
+			
+			int numElementsPage = Integer.valueOf(driver.findElement(By.xpath("//*[@id='objectsPerPage']")).findElements(By.tagName("option")).get(i).getAttribute("value"));
+			
+			if (Integer.valueOf(numExpedientes) >= numElementsPage) {
+				assertTrue("El número de expedientes por página no coincidía", numElementsPage == driver.findElements(By.xpath("//*[@id='registre']/tbody/tr")).size());
+			} else {
+				assertTrue("El número de expedientes encontrados era mayor a la página", numElementsPage > driver.findElements(By.xpath("//*[@id='registre']/tbody/tr")).size());
+			}
+			
+			i++;
+		}
+	}
+
+	@Test
+	public void f_desplegar_tasques_expedient() throws InterruptedException, ParseException {	
+		carregarUrlConfiguracio();
+		
+		seleccionarEntorno(entorn);
+		
+		// Asignamos permisos
+		assignarPermisosTipusExpedient(codTipusExp, usuari, "DESIGN","CREATE","SUPERVISION","WRITE","MANAGE","DELETE","READ","ADMINISTRATION");
+		
+		consultarExpedientes(null, null, tipusExp);
+		
+		existeixElementAssert("//*[@id='registre']/tbody/tr[1]/td[2]/a", "No se tenía ningún expediente");
+		
+		// Vamos a la tarea a tramitar del expediente
+		String nomExpediente = driver.findElement(By.xpath("//*[@id='registre']/tbody/tr[1]/td[2]/a")).getText();
+		driver.findElement(By.xpath("//*[@id='registre']/tbody/tr[1]/td[2]/a")).click();
+		
+		assertTrue("No se llegó a la pantalla de tasques pendents", "Tasques pendents".equals(driver.findElement(By.xpath("//*[@id='page-title']/h2/span")).getText()));
+		assertTrue("No se realizó la búsqueda con el nombre del expediente correcto", nomExpediente.equals(driver.findElement(By.xpath("//*[@id='expedient0']")).getAttribute("value")));
+	}
+	
+	@Test
+	public void h_acces_a_tramitacio_de_tasca() throws InterruptedException, ParseException {	
+
+		carregarUrlConfiguracio();
+		
+		seleccionarEntorno(entorn);
+		
+		// Asignamos permisos
+		assignarPermisosTipusExpedient(codTipusExp, usuari, "DESIGN","CREATE","SUPERVISION","WRITE","MANAGE","DELETE","READ","ADMINISTRATION");
+		
+		consultarExpedientes(null, null, tipusExp);
+		
+		existeixElementAssert("//*[@id='registre']/tbody/tr[1]/td[2]/a", "No se tenía ningún expediente");
+		
+		// Vamos a la tarea a tramitar del expediente
+		String nomExpediente = driver.findElement(By.xpath("//*[@id='registre']/tbody/tr[1]/td[2]/a")).getText();
+		driver.findElement(By.xpath("//*[@id='registre']/tbody/tr[1]/td[2]/a")).click();
+		
+		assertTrue("No se llegó a la pantalla de tasques pendents", "Tasques pendents".equals(driver.findElement(By.xpath("//*[@id='page-title']/h2/span")).getText()));
+		assertTrue("No se realizó la búsqueda con el nombre del expediente correcto", nomExpediente.equals(driver.findElement(By.xpath("//*[@id='expedient0']")).getAttribute("value")));
+		
+		if (existeixElement("//*[@id='registre']/tbody/tr[1]/td[1]/a")) {
+			driver.findElement(By.xpath("//*[@id='registre']/tbody/tr[1]/td[1]/a")).click();
+			
+			//*[@id='content']/h3
+			assertTrue("No se llegó a la pantalla de tasques pendents", "Tasques pendents".equals(driver.findElement(By.xpath("//*[@id='page-title']/h2/span")).getText()));
+			assertTrue("No se realizó la búsqueda con el nombre del expediente correcto", nomExpediente.equals(driver.findElement(By.xpath("//*[@id='expedient0']")).getAttribute("value")));
+		}
 	}
 
 	@Test
@@ -213,33 +443,6 @@ public class Expedient extends BaseTest {
 		consultarExpedientes(null, null, tipusExp);
 		
 		existeixElementAssert("//*[@id='registre']/tbody/tr[1]/td/img[@src = '/helium/img/stop.png']", "No exitía la imagen de expediente aturado");
-	}
-	
-	@Test
-	public void m_anular_expedient() throws InterruptedException, ParseException {	
-		carregarUrlConfiguracio();
-		
-		seleccionarEntorno(entorn);
-		
-		// Comprobamos permisos de borrado
-		
-		// Los quitamos
-		assignarPermisosTipusExpedient(codTipusExp, usuari, "DESIGN","CREATE","SUPERVISION","DELETE","MANAGE","READ");
-		
-		consultarExpedientes(null, null, tipusExp);
-		
-		noExisteixElementAssert("//*[@id='registre']/tbody/tr[1]/td[contains(a/img/@src,'/helium/img/delete.png')]/a/img", "Tenía permisos de anulado");
-		
-		// Los ponemos
-		assignarPermisosTipusExpedient(codTipusExp, usuari, "DESIGN","CREATE","SUPERVISION","WRITE","MANAGE","DELETE","READ","ADMINISTRATION");
-		
-		consultarExpedientes(null, null, tipusExp);
-		
-		existeixElementAssert("//*[@id='registre']/tbody/tr[1]/td[contains(a/img/@src,'/helium/img/delete.png')]/a/img", "No tenía permisos de anulado");
-		
-		// Anulamos el primer expediente
-		driver.findElement(By.xpath("//*[@id='registre']/tbody/tr[1]/td[7]/a")).click();
-		acceptarConfirm("El motivo");
 	}
 
 	@Test
