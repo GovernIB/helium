2e97818c3e3248bef6e45267bc45b7e5cda6e474
sandreu-limit <sandreu-limit@e3271b2b-fd41-4d56-bedf-4b5636213925>
2014-07-09 14:55:52 +0000

    MID: 655 MSG: Jocs de proves amb Selelium v.3
    git-svn-id: svn://svn.code.sf.net/p/gehelium/code/helium/branches/helium-3.0@1291 e3271b2b-fd41-4d56-bedf-4b5636213925

--- helium-selenium-integraciones/src/net/java/dev/jaxb/array/AnyTypeArrayArray.java
+++ helium-selenium-integraciones/src/net/java/dev/jaxb/array/AnyTypeArrayArray.java
@@ -3,6 +3,7 @@ package net.java.dev.jaxb.array;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
