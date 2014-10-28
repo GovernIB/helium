3e2c34e0570a9b55f68329f88f3679c659871e6c
josepg-limit <josepg-limit@e3271b2b-fd41-4d56-bedf-4b5636213925>
2014-10-28 09:51:05 +0000

    MID: 702 MSG: NullPointerException al emmagatzemar variables dels formularis externs
    git-svn-id: svn://svn.code.sf.net/p/gehelium/code/helium/branches/helium-3.0@1419 e3271b2b-fd41-4d56-bedf-4b5636213925

--- helium-ws/src/main/java/net/conselldemallorca/helium/ws/formulari/GuardarFormulariImpl.java
+++ helium-ws/src/main/java/net/conselldemallorca/helium/ws/formulari/GuardarFormulariImpl.java
@@ -30,21 +30,23 @@ public class GuardarFormulariImpl implements GuardarFormulari {
 
 	public void guardar(String formulariId, List<ParellaCodiValor> valors) {
 		Map<String, Object> valorsTasca = new HashMap<String, Object>();
-		for (ParellaCodiValor parella: valors) {
-			/*String tipus = null;
-			if (parella.getValor() != null)
-				tipus = parella.getValor().getClass().getName();
-			System.out.println(">>> Variable " + parella.getCodi() + ": " + tipus);*/
-			if (parella.getValor() != null) {
-				if (parella.getValor() instanceof XMLGregorianCalendar) {
-					valorsTasca.put(
-							parella.getCodi(),
-							((XMLGregorianCalendar)parella.getValor()).toGregorianCalendar().getTime());
+		if (valors != null) {
+			for (ParellaCodiValor parella: valors) {
+				/*String tipus = null;
+				if (parella.getValor() != null)
+					tipus = parella.getValor().getClass().getName();
+				System.out.println(">>> Variable " + parella.getCodi() + ": " + tipus);*/
+				if (parella.getValor() != null) {
+					if (parella.getValor() instanceof XMLGregorianCalendar) {
+						valorsTasca.put(
+								parella.getCodi(),
+								((XMLGregorianCalendar)parella.getValor()).toGregorianCalendar().getTime());
+					} else {
+						valorsTasca.put(parella.getCodi(), parella.getValor());
+					}
 				} else {
 					valorsTasca.put(parella.getCodi(), parella.getValor());
 				}
-			} else {
-				valorsTasca.put(parella.getCodi(), parella.getValor());
 			}
 		}
 		tascaService.guardarFormulariExtern(formulariId, valorsTasca);
