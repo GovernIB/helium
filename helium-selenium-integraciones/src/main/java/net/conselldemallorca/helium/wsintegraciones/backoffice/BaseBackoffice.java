a2c334aa60cb68f110defdbb707d4c50a95b32a5
sandreu-limit <sandreu-limit@e3271b2b-fd41-4d56-bedf-4b5636213925>
2014-12-12 08:01:02 +0000

    MID: 655. MSG: Jocs de proves amb Selelium
    git-svn-id: svn://svn.code.sf.net/p/gehelium/code/helium/branches/helium-3.0@1466 e3271b2b-fd41-4d56-bedf-4b5636213925

--- helium-selenium-integraciones/src/main/java/net/conselldemallorca/helium/wsintegraciones/backoffice/BaseBackoffice.java
+++ helium-selenium-integraciones/src/main/java/net/conselldemallorca/helium/wsintegraciones/backoffice/BaseBackoffice.java
@@ -334,7 +334,7 @@ public abstract class BaseBackoffice {
 				    if (node.hasAttribute("indice")) {
 						return node.getAttribute("indice");
 					} else if (node.getChildNodes().getLength() == 1) {
-						return node.getNodeValue();
+						return node.getTextContent();
 					} else {
 						List<String[]> valorsFiles = new ArrayList<String[]>();
 						NodeList nodes = node.getChildNodes();
@@ -348,7 +348,7 @@ public abstract class BaseBackoffice {
 									if (columna.hasAttribute("indice"))
 										valors[i] = columna.getAttribute("indice");
 									else
-										valors[i] = columna.getNodeValue();
+										valors[i] = columna.getTextContent();
 								}
 								valorsFiles.add(valors);
 							}
