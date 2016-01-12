d27069bb99ae47239026ddfbdb822aacb9fd9095
josepg-limit <josepg-limit@e3271b2b-fd41-4d56-bedf-4b5636213925>
2016-01-12 14:54:26 +0000

    MID: 0000719 MSG: Solucionar problemes amb formularis externs
    git-svn-id: svn://svn.code.sf.net/p/gehelium/code/helium/branches/helium-3.0@1733 e3271b2b-fd41-4d56-bedf-4b5636213925

--- helium-core/src/main/java/net/conselldemallorca/helium/v3/core/helper/FormulariExternHelper.java
+++ helium-core/src/main/java/net/conselldemallorca/helium/v3/core/helper/FormulariExternHelper.java
@@ -3,31 +3,27 @@
  */
 package net.conselldemallorca.helium.v3.core.helper;
 
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Resource;
-import javax.xml.namespace.QName;
-import javax.xml.ws.BindingProvider;
-import javax.xml.ws.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import net.conselldemallorca.helium.core.model.hibernate.ExpedientTipus;
 import net.conselldemallorca.helium.core.model.hibernate.FormulariExtern;
 import net.conselldemallorca.helium.core.model.hibernate.Tasca;
+import net.conselldemallorca.helium.core.model.service.TascaService;
 import net.conselldemallorca.helium.core.util.GlobalProperties;
 import net.conselldemallorca.helium.v3.core.api.dto.FormulariExternDto;
-import net.conselldemallorca.helium.v3.core.api.exception.SistemaExternException;
+import net.conselldemallorca.helium.v3.core.helper.WsClientHelper.WsClientAuth;
 import net.conselldemallorca.helium.v3.core.repository.FormulariExternRepository;
 import net.conselldemallorca.helium.v3.core.ws.formext.IniciFormulari;
 import net.conselldemallorca.helium.v3.core.ws.formext.ParellaCodiValor;
 import net.conselldemallorca.helium.v3.core.ws.formext.RespostaIniciFormulari;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.springframework.stereotype.Component;
 
 /**
  * Helper per a accedir a la funcionalitat dels plugins.
@@ -42,178 +38,96 @@ public class FormulariExternHelper {
 	@Resource
 	private TascaHelper tascaHelper;
 	@Resource
+	private VariableHelper variableHelper;
+	@Resource
 	private FormulariExternRepository formulariExternRepository;
+	@Autowired
+	private WsClientHelper wsClientHelper;
 	
 
 	public FormulariExternDto iniciar(
 			String taskId,
 			Map<String, Object> valors) {
-		try {
-			ExpedientTipus expedientTipus = expedientTipusHelper.findAmbTaskId(
-					taskId);
-			String url;
-			String username = null;
-			String password = null;
-			if (expedientTipus.getFormextUrl() != null) {
-				url = expedientTipus.getFormextUrl();
-				if (expedientTipus.getFormextUsuari() != null)
-					username = expedientTipus.getFormextUsuari();
-				if (expedientTipus.getFormextContrasenya() != null)
-					password = expedientTipus.getFormextContrasenya();
-			} else {
-				url = GlobalProperties.getInstance().getProperty(
-						"app.forms.service.url");
-				username = GlobalProperties.getInstance().getProperty(
-						"app.forms.service.username");
-				password = GlobalProperties.getInstance().getProperty(
-						"app.forms.service.password");
-			}
-			List<ParellaCodiValor> varsFormext = new ArrayList<ParellaCodiValor>();
-			for (String var: valors.keySet()) {
-				varsFormext.add(
-						new ParellaCodiValor(
-								var,
-								valors.get(var)));
-			}
-			
-			
-			Tasca tasca = tascaHelper.findTascaByJbpmTaskId(taskId);
-			IniciFormulari service = getIniciFormulariService(
-					url,
-					username,
-					password);
-			RespostaIniciFormulari resposta = service.iniciFormulari(
-					tasca.getFormExtern(),
-					taskId,
-					varsFormext);
-			
-			
-			
-//			RespostaIniciFormulari resposta = new RespostaIniciFormulari();
-//			resposta.setFormulariId(taskId);
-//			resposta.setUrl("http://oficina.limit.es");
-//			resposta.setWidth(800);
-//			resposta.setHeight(600);
-			 
-			FormulariExtern fext = formulariExternRepository.findByFormulariId(resposta.getFormulariId());
-			if (fext == null) {
-				fext = new FormulariExtern(
-						taskId,
-						resposta.getFormulariId(),
-						resposta.getUrl());
-				formulariExternRepository.save(fext);
-			} else {
-				fext.setUrl(resposta.getUrl());
-				fext.setDataDarreraPeticio(new Date());
-				if (resposta.getWidth() != -1)
-					fext.setFormWidth(resposta.getWidth());
-				if (resposta.getHeight() != -1)
-					fext.setFormHeight(resposta.getHeight());
-			}
-			FormulariExternDto dto = new FormulariExternDto();
-			dto.setFormulariId(resposta.getFormulariId());
-			dto.setUrl(resposta.getUrl());
-			dto.setWidth(resposta.getWidth());
-			dto.setHeight(resposta.getHeight());
-			return dto;
-		} catch (Exception ex) {
-			logger.error("Error al iniciar formulariExtern", ex);
-			throw new SistemaExternException("Error al iniciar formulariExtern", ex);
-		}
+		ExpedientTipus expedientTipus = expedientTipusHelper.findAmbTaskId(
+				taskId);
+		Tasca tasca = tascaHelper.findTascaByJbpmTaskId(taskId);
+		return iniciar(
+				taskId,
+				tasca,
+				expedientTipus);
 	}
 	
 	public FormulariExternDto iniciar(
-			String taskId,
-			Tasca tasca,
-			ExpedientTipus expedientTipus) {
-		try {
-			String url;
-			String username = null;
-			String password = null;
-			if (expedientTipus.getFormextUrl() != null) {
-				url = expedientTipus.getFormextUrl();
-				if (expedientTipus.getFormextUsuari() != null)
-					username = expedientTipus.getFormextUsuari();
-				if (expedientTipus.getFormextContrasenya() != null)
-					password = expedientTipus.getFormextContrasenya();
-			} else {
-				url = GlobalProperties.getInstance().getProperty(
-						"app.forms.service.url");
-				username = GlobalProperties.getInstance().getProperty(
-						"app.forms.service.username");
-				password = GlobalProperties.getInstance().getProperty(
-						"app.forms.service.password");
-			}
-			
-			/*
-			IniciFormulari service = getIniciFormulariService(
-					url,
-					username,
-					password);
-			RespostaIniciFormulari resposta = service.iniciFormulari(
-					tasca.getFormExtern(),
+		String taskId,
+		Tasca tasca,
+		ExpedientTipus expedientTipus) {
+		String url;
+		String username = null;
+		String password = null;
+		if (expedientTipus.getFormextUrl() != null) {
+			url = expedientTipus.getFormextUrl();
+			if (expedientTipus.getFormextUsuari() != null)
+				username = expedientTipus.getFormextUsuari();
+			if (expedientTipus.getFormextContrasenya() != null)
+				password = expedientTipus.getFormextContrasenya();
+		} else {
+			url = GlobalProperties.getInstance().getProperty(
+					"app.forms.service.url");
+			username = GlobalProperties.getInstance().getProperty(
+					"app.forms.service.username");
+			password = GlobalProperties.getInstance().getProperty(
+					"app.forms.service.password");
+		}
+		IniciFormulari clientWs = wsClientHelper.getIniciFormulariService(
+				url,
+				WsClientAuth.NONE,
+				username,
+				password);
+		RespostaIniciFormulari resposta = clientWs.iniciFormulari(
+				tasca.getFormExtern(),
+				taskId,
+				getVariablesPerIniciFormulari(taskId));
+		/*RespostaIniciFormulari resposta = new RespostaIniciFormulari();
+		resposta.setFormulariId(taskId);
+		resposta.setUrl("http://oficina.limit.es");
+		resposta.setWidth(800);
+		resposta.setHeight(600);*/
+		FormulariExtern fext = formulariExternRepository.findByFormulariId(resposta.getFormulariId());
+		if (fext == null) {
+			fext = new FormulariExtern(
 					taskId,
-					varsFormext);
-			*/
-			
-			
-			RespostaIniciFormulari resposta = new RespostaIniciFormulari();
-			resposta.setFormulariId(taskId);
-			resposta.setUrl("http://oficina.limit.es");
-			resposta.setWidth(800);
-			resposta.setHeight(600);
-			 
-			FormulariExtern fext = formulariExternRepository.findByFormulariId(resposta.getFormulariId());
-			if (fext == null) {
-				fext = new FormulariExtern(
-						taskId,
-						resposta.getFormulariId(),
-						resposta.getUrl());
-				formulariExternRepository.save(fext);
-			} else {
-				fext.setUrl(resposta.getUrl());
-				fext.setDataDarreraPeticio(new Date());
-				if (resposta.getWidth() != -1)
-					fext.setFormWidth(resposta.getWidth());
-				if (resposta.getHeight() != -1)
-					fext.setFormHeight(resposta.getHeight());
+					resposta.getFormulariId(),
+					resposta.getUrl());
+			formulariExternRepository.save(fext);
+		} else {
+			fext.setUrl(resposta.getUrl());
+			fext.setDataDarreraPeticio(new Date());
+			if (resposta.getWidth() != -1)
+				fext.setFormWidth(resposta.getWidth());
+			if (resposta.getHeight() != -1)
+				fext.setFormHeight(resposta.getHeight());
+		}
+		FormulariExternDto dto = new FormulariExternDto();
+		dto.setFormulariId(resposta.getFormulariId());
+		dto.setUrl(resposta.getUrl());
+		dto.setWidth(resposta.getWidth());
+		dto.setHeight(resposta.getHeight());
+		return dto;
+	}
+
+
+
+	private List<ParellaCodiValor> getVariablesPerIniciFormulari(
+			String taskId) {
+		Map<String, Object> varsTasca = variableHelper.getVariablesJbpmTascaValor(taskId);
+		List<ParellaCodiValor> varsForm = new ArrayList<ParellaCodiValor>();
+		if (varsTasca != null) {
+			for (String key: varsTasca.keySet()) {
+				if (!key.startsWith(TascaService.VAR_PREFIX))
+					varsForm.add(new ParellaCodiValor(key, varsTasca.get(key)));
 			}
-			FormulariExternDto dto = new FormulariExternDto();
-			dto.setFormulariId(resposta.getFormulariId());
-			dto.setUrl(resposta.getUrl());
-			dto.setWidth(resposta.getWidth());
-			dto.setHeight(resposta.getHeight());
-			return dto;
-		} catch (Exception ex) {
-			logger.error("Error al iniciar formulariExtern", ex);
-			throw new SistemaExternException("Error al iniciar formulariExtern", ex);
 		}
+		return varsForm;
 	}
-
-
-
-	private IniciFormulari getIniciFormulariService(
-			String serviceUrl,
-			String username,
-			String password) throws Exception {
-		Service service = Service.create(
-				new URL(serviceUrl),
-				new QName(
-						"http://caib.es/emiserv/backoffice",
-						"EmiservBackofficeService"));
-		IniciFormulari port = (IniciFormulari)service.getPort(
-				new QName(
-						"http://caib.es/emiserv/backoffice",
-						"EmiservBackofficePort"),
-				IniciFormulari.class);
-		if (username != null && !username.isEmpty()) {
-			BindingProvider bp = (BindingProvider)port;
-			bp.getRequestContext().put(BindingProvider.USERNAME_PROPERTY, username);
-			bp.getRequestContext().put(BindingProvider.PASSWORD_PROPERTY, password);
-		}
-		return port;
-	}
-
-	private static final Log logger = LogFactory.getLog(FormulariExternHelper.class);
 
 }
---
+++ helium-core/src/main/java/net/conselldemallorca/helium/v3/core/helper/WsClientHelper.java
@@ -0,0 +1,98 @@
+/**
+ * 
+ */
+package net.conselldemallorca.helium.v3.core.helper;
+
+import org.springframework.stereotype.Component;
+
+import net.conselldemallorca.helium.core.extern.domini.DominiHelium;
+import net.conselldemallorca.helium.core.util.ws.WsClientUtils;
+import net.conselldemallorca.helium.v3.core.ws.formext.IniciFormulari;
+
+/**
+ * Helper per a obtenir els clients de serveis web externs.
+ * 
+ * @author Limit Tecnologies <limit@limit.es>
+ */
+@Component
+public class WsClientHelper {
+
+	public enum WsClientAuth {
+		NONE,
+		BASIC,
+		USERNAMETOKEN
+	}
+
+	public DominiHelium getDominiService(
+			String serviceUrl,
+			WsClientAuth auth,
+			String username,
+			String password) {
+		return (DominiHelium)WsClientUtils.getWsClientProxy(
+				DominiHelium.class,
+				serviceUrl,
+				username,
+				password,
+				getAuthAsString(auth),
+				false,
+				false,
+				true);
+	}
+
+	/*public IniciFormulari getIniciFormulariService(
+			String serviceUrl,
+			WsClientAuth auth,
+			String username,
+			String password) throws Exception {
+		Service service = Service.create(
+				new URL(serviceUrl),
+				new QName(
+						"http://forms.integracio.helium.conselldemallorca.net/",
+						"IniciFormulariService"));
+		IniciFormulari port = (IniciFormulari)service.getPort(
+				new QName(
+						"http://forms.integracio.helium.conselldemallorca.net/",
+						"IniciFormulariPort"),
+				IniciFormulari.class);
+		if (username != null && !username.isEmpty()) {
+			BindingProvider bp = (BindingProvider)port;
+			bp.getRequestContext().put(BindingProvider.USERNAME_PROPERTY, username);
+			bp.getRequestContext().put(BindingProvider.PASSWORD_PROPERTY, password);
+		}
+		return port;
+	}*/
+	public IniciFormulari getIniciFormulariService(
+			String serviceUrl,
+			WsClientAuth auth,
+			String username,
+			String password) {
+		return (IniciFormulari)WsClientUtils.getWsClientProxy(
+				IniciFormulari.class,
+				serviceUrl,
+				username,
+				password,
+				getAuthAsString(auth),
+				false,
+				false,
+				true);
+	}
+
+
+
+	private String getAuthAsString(WsClientAuth auth) {
+		String authStr;
+		switch(auth) {
+		case BASIC:
+			authStr = "BASIC";
+			break;
+		case USERNAMETOKEN:
+			authStr = "USERNAMETOKEN";
+			break;
+		default:
+			authStr = "NONE";
+			break;
+		}
+		return authStr;
+	}
+
+}
