480b75ed07606b767f1332aacba657e4bf1dbf90
josepg-limit <josepg-limit@e3271b2b-fd41-4d56-bedf-4b5636213925>
2016-03-08 16:51:28 +0000

    MID: 0000776 MSG: Error al generar documents de tasca a partir de plantilles.
    git-svn-id: svn://svn.code.sf.net/p/gehelium/code/helium/branches/helium-3.0@1852 e3271b2b-fd41-4d56-bedf-4b5636213925

--- helium-core/src/main/java/net/conselldemallorca/helium/v3/core/helper/PlantillaHelper.java
+++ helium-core/src/main/java/net/conselldemallorca/helium/v3/core/helper/PlantillaHelper.java
@@ -424,7 +424,7 @@ public class PlantillaHelper {
 				"personaAmbCarrecArea",
 				new TemplateMethodModel() {
 					public TemplateModel exec(List args) throws TemplateModelException {
-						if (args.size() == 1) {
+						if (args.size() == 2) {
 							Object arg0 = args.get(0);
 							Object arg1 = args.get(1);
 							if (arg0 != null && arg0 instanceof String && arg1 != null && arg1 instanceof String) {
