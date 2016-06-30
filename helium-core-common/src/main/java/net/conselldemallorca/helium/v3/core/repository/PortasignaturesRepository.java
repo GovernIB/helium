358ff75e72af4f2ec4b3430a2e985d747788e565
Josep Gayà <josepg@limit.es>
2016-06-30 14:46:22 +0200

    851: Error 'could not execute query' esborrant document de l'expedient
    Task-Url: http://github.com/GovernIB/helium/issues/issue/851

--- helium-core-common/src/main/java/net/conselldemallorca/helium/v3/core/repository/PortasignaturesRepository.java
+++ helium-core-common/src/main/java/net/conselldemallorca/helium/v3/core/repository/PortasignaturesRepository.java
@@ -35,10 +35,7 @@ public interface PortasignaturesRepository extends JpaRepository<Portasignatures
 			"    Portasignatures p " +
 			"where " +
 			"    p.processInstanceId = :processInstanceId " +
-			"and p.estat not in :estats <> 'PENDENT' " +
-			"and p.estat <> 'SIGNAT' " +
-			"and p.estat <> 'REBUTJAT' " +
-			"and p.estat <> 'ERROR' ")
+			"and p.estat not in :estats ")
 	List<Portasignatures> findByProcessInstanceIdAndEstatNotIn(
 			@Param("processInstanceId") String processInstanceId,
 			@Param("estat") TipusEstat[] estats);
