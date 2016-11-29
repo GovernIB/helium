799fbaee0774d679654620e2ff939b6c436f6e84
Josep Gayà <josepg@limit.es>
2016-11-29 13:53:21 +0100

    DISSENY JAR ACTUALITZAT

--- helium-disseny/src/main/java/net/conselldemallorca/helium/jbpm3/handlers/ZonaperEventNotificacioHandler.java
+++ helium-disseny/src/main/java/net/conselldemallorca/helium/jbpm3/handlers/ZonaperEventNotificacioHandler.java
@@ -11,50 +11,255 @@ import org.jbpm.graph.exe.ExecutionContext;
  */
 
 @SuppressWarnings("serial")
-public class ZonaperEventNotificacioHandler extends BasicActionHandler implements RegistreSortidaHandlerInterface, AccioExternaRetrocedirHandler  {
+public class ZonaperEventNotificacioHandler extends BasicActionHandler
+		implements RegistreSortidaHandlerInterface, AccioExternaRetrocedirHandler {
 
-	public ZonaperEventNotificacioHandler() {}
-	public void setOficina(String oficina) {}
-	public void setVarOficina(String varOficina) {}
-	public void setOficinaFisica(String oficinaFisica) {}
-	public void setVarOficinaFisica(String varOficinaFisica) {}
-	public void setRemitentCodiEntitat(String remitentCodiEntitat) {}
-	public void setVarRemitentCodiEntitat(String varRemitentCodiEntitat) {}
-	public void setRemitentNomEntitat(String remitentNomEntitat) {}
-	public void setVarRemitentNomEntitat(String varRemitentNomEntitat) {}
-	public void setRemitentCodiGeografic(String remitentCodiGeografic) {}
-	public void setVarRemitentCodiGeografic(String varRemitentCodiGeografic) {}
-	public void setRemitentNomGeografic(String remitentNomGeografic) {}
-	public void setVarRemitentNomGeografic(String varRemitentNomGeografic) {}
-	public void setRemitentRegistreNumero(String remitentRegistreNumero) {}
-	public void setVarRemitentRegistreNumero(String varRemitentRegistreNumero) {}
-	public void setRemitentRegistreAny(String remitentRegistreAny) {}
-	public void setVarRemitentRegistreAny(String varRemitentRegistreAny) {}
-	public void setDestinatariCodiEntitat(String destinatariCodiEntitat) {}
-	public void setVarDestinatariCodiEntitat(String varDestinatariCodiEntitat) {}
-	public void setDestinatariNomEntitat(String destinatariNomEntitat) {}
-	public void setVarDestinatariNomEntitat(String varDestinatariNomEntitat) {}
-	public void setDestinatariCodiGeografic(String destinatariCodiGeografic) {}
-	public void setVarDestinatariCodiGeografic(String varDestinatariCodiGeografic) {}
-	public void setDestinatariNomGeografic(String destinatariNomGeografic) {}
-	public void setVarDestinatariNomGeografic(String varDestinatariNomGeografic) {}
-	public void setDestinatariRegistreNumero(String destinatariRegistreNumero) {}
-	public void setVarDestinatariRegistreNumero(String varDestinatariRegistreNumero) {}
-	public void setDestinatariRegistreAny(String destinatariRegistreAny) {}
-	public void setVarDestinatariRegistreAny(String varDestinatariRegistreAny) {}
-	public void setDocumentTipus(String documentTipus) {}
-	public void setVarDocumentTipus(String varDocumentTipus) {}
-	public void setDocumentIdiomaDocument(String documentIdiomaDocument) {}
-	public void setVarDocumentIdiomaDocument(String varDocumentIdiomaDocument) {}
-	public void setDocumentIdiomaExtracte(String documentIdiomaExtracte) {}
-	public void setVarDocumentIdiomaExtracte(String varDocumentIdiomaExtracte) {}
-	public void setVarDocument(String varDocument) {}
-	public void setVarNumeroRegistre(String varNumeroRegistre) {}
-	public void setVarNumeroAnyRegistre(String varNumeroAnyRegistre) {}
-	public void setVarDataRegistre(String varDataRegistre) {}
-	public void setVarData(String varData) {}
-	
-	public void execute(ExecutionContext executionContext) throws Exception {}
-	public void retrocedir(ExecutionContext context, List<String> parametres) throws Exception {}
+	public ZonaperEventNotificacioHandler() {
+	}
 
+	public void execute(ExecutionContext executionContext) throws Exception {
+	}
+
+	public void retrocedir(ExecutionContext context, List<String> parametres) throws Exception {
+	}
+
+	public void setRepresentatNif(String representatNif) {
+	}
+
+	public void setVarRepresentatNif(String varRepresentatNif) {
+	}
+
+	public void setRepresentatNom(String representatNom) {
+	}
+
+	public void setVarRepresentatNom(String varRepresentatNom) {
+	}
+
+	public void setRepresentatLlin1(String representatLlin1) {
+	}
+
+	public void setVarRepresentatLlin1(String varRepresentatLlin1) {
+	}
+
+	public void setRepresentatLlin2(String representatLlin2) {
+	}
+
+	public void setVarRepresentatLlin2(String varRepresentatLlin2) {
+	}
+
+	public void setOficina(String oficina) {
+	}
+
+	public void setVarOficina(String varOficina) {
+	}
+
+	public void setOficinaFisica(String oficinaFisica) {
+	}
+
+	public void setVarOficinaFisica(String varOficinaFisica) {
+	}
+
+	public void setRemitentCodiEntitat(String remitentCodiEntitat) {
+	}
+
+	public void setVarRemitentCodiEntitat(String varRemitentCodiEntitat) {
+	}
+
+	public void setRemitentNomEntitat(String remitentNomEntitat) {
+	}
+
+	public void setVarRemitentNomEntitat(String varRemitentNomEntitat) {
+	}
+
+	public void setRemitentCodiGeografic(String remitentCodiGeografic) {
+	}
+
+	public void setVarRemitentCodiGeografic(String varRemitentCodiGeografic) {
+	}
+
+	public void setRemitentNomGeografic(String remitentNomGeografic) {
+	}
+
+	public void setVarRemitentNomGeografic(String varRemitentNomGeografic) {
+	}
+
+	public void setRemitentRegistreNumero(String remitentRegistreNumero) {
+	}
+
+	public void setVarRemitentRegistreNumero(String varRemitentRegistreNumero) {
+	}
+
+	public void setRemitentRegistreAny(String remitentRegistreAny) {
+	}
+
+	public void setVarRemitentRegistreAny(String varRemitentRegistreAny) {
+	}
+
+	public void setDestinatariCodiEntitat(String destinatariCodiEntitat) {
+	}
+
+	public void setVarDestinatariCodiEntitat(String varDestinatariCodiEntitat) {
+	}
+
+	public void setDestinatariNomEntitat(String destinatariNomEntitat) {
+	}
+
+	public void setVarDestinatariNomEntitat(String varDestinatariNomEntitat) {
+	}
+
+	public void setDestinatariCodiGeografic(String destinatariCodiGeografic) {
+	}
+
+	public void setVarDestinatariCodiGeografic(String varDestinatariCodiGeografic) {
+	}
+
+	public void setDestinatariNomGeografic(String destinatariNomGeografic) {
+	}
+
+	public void setVarDestinatariNomGeografic(String varDestinatariNomGeografic) {
+	}
+
+	public void setDestinatariCodiProvincia(String destinatariCodiProvincia) {
+	}
+
+	public void setVarDestinatariCodiProvincia(String varDestinatariCodiProvincia) {
+	}
+
+	public void setDestinatariNomProvincia(String destinatariNomProvincia) {
+	}
+
+	public void setVarDestinatariNomProvincia(String varDestinatariNomProvincia) {
+	}
+
+	public void setDestinatariCodiPais(String destinatariCodiPais) {
+	}
+
+	public void setVarDestinatariCodiPais(String varDestinatariCodiPais) {
+	}
+
+	public void setDestinatariNomPais(String destinatariNomPais) {
+	}
+
+	public void setVarDestinatariNomPais(String varDestinatariNomPais) {
+	}
+
+	public void setDestinatariRegistreNumero(String destinatariRegistreNumero) {
+	}
+
+	public void setVarDestinatariRegistreNumero(String varDestinatariRegistreNumero) {
+	}
+
+	public void setDestinatariRegistreAny(String destinatariRegistreAny) {
+	}
+
+	public void setVarDestinatariRegistreAny(String varDestinatariRegistreAny) {
+	}
+
+	public void setDocumentTipus(String documentTipus) {
+	}
+
+	public void setVarDocumentTipus(String varDocumentTipus) {
+	}
+
+	public void setDocumentIdiomaDocument(String documentIdiomaDocument) {
+	}
+
+	public void setVarDocumentIdiomaDocument(String varDocumentIdiomaDocument) {
+	}
+
+	public void setDocumentIdiomaExtracte(String documentIdiomaExtracte) {
+	}
+
+	public void setVarDocumentIdiomaExtracte(String varDocumentIdiomaExtracte) {
+	}
+
+	public void setDocument(String document) {
+	}
+
+	public void setVarDocument(String varDocument) {
+	}
+
+	public void setAnnexos(String annexos) {
+	}
+
+	public void setVarAnnexos(String varAnnexos) {
+	}
+
+	public void setVarNumeroRegistre(String varNumeroRegistre) {
+	}
+
+	public void setVarNumeroAnyRegistre(String varNumeroAnyRegistre) {
+	}
+
+	public void setVarDataRegistre(String varDataRegistre) {
+	}
+
+	public void setVarData(String varData) {
+	}
+
+	public void setVarReferenciaRDSJustificanteClave(String varReferenciaRDSJustificanteClave) {
+	}
+
+	public void setVarReferenciaRDSJustificanteCodigo(String varReferenciaRDSJustificanteCodigo) {
+	}
+
+	public void setUnitatAdministrativa(String unitatAdministrativa) {
+	}
+
+	public void setVarUnitatAdministrativa(String varUnitatAdministrativa) {
+	}
+
+	public void setAvisTitol(String avisTitol) {
+	}
+
+	public void setVarAvisTitol(String varAvisTitol) {
+	}
+
+	public void setAvisText(String avisText) {
+	}
+
+	public void setVarAvisText(String varAvisText) {
+	}
+
+	public void setAvisTextSms(String avisTextSms) {
+	}
+
+	public void setVarAvisTextSms(String varAvisTextSms) {
+	}
+
+	public void setNotificacioOficiTitol(String notificacioOficiTitol) {
+	}
+
+	public void setVarNotificacioOficiTitol(String varNotificacioOficiTitol) {
+	}
+
+	public void setNotificacioOficiText(String notificacioOficiText) {
+	}
+
+	public void setVarNotificacioOficiText(String varNotificacioOficiText) {
+	}
+
+	public void setNotificacioSubsanacioTramitIdentificador(String notificacioSubsanacioTramitIdentificador) {
+	}
+
+	public void setVarNotificacioSubsanacioTramitIdentificador(String varNotificacioSubsanacioTramitIdentificador) {
+	}
+
+	public void setNotificacioSubsanacioTramitVersio(int notificacioSubsanacioTramitVersio) {
+	}
+
+	public void setVarNotificacioSubsanacioTramitVersio(String varNotificacioSubsanacioTramitVersio) {
+	}
+
+	public void setNotificacioSubsanacioTramitDescripcio(String notificacioSubsanacioTramitDescripcio) {
+	}
+
+	public void setVarNotificacioSubsanacioTramitDescripcio(String varNotificacioSubsanacioTramitDescripcio) {
+	}
+
+	public void setNotificacioCrearExpedient(boolean notificacioCrearExpedient) {
+	}
+
+	public void setVarNotificacioCrearExpedient(String varNotificacioCrearExpedient) {
+	}
 }
