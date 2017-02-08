acdee74dd2255ae99f623fd9acf29bd9d0dffef8
Josep Gayà <josepg@limit.es>
2017-02-08 17:30:39 +0100

    #981: Millora reindexacio asincrona
    Task-Url: https://github.com/GovernIB/helium/issues/issue/981
    
    NOVA RELEASE: 3.1.92

--- helium-core-api/src/main/java/net/conselldemallorca/helium/v3/core/api/service/TascaProgramadaService.java
+++ helium-core-api/src/main/java/net/conselldemallorca/helium/v3/core/api/service/TascaProgramadaService.java
@@ -7,5 +7,8 @@ import net.conselldemallorca.helium.v3.core.api.exception.NoTrobatException;
 public interface TascaProgramadaService {
 	public void comprovarExecucionsMassives() throws NoTrobatException, ExecucioMassivaException;
 	public void comprovarReindexacioAsincrona() throws NoTrobatException;
-	public void actualitzarEstatNotificacions() throws NoTrobatException;
+	public void reindexarExpedient (Long expedientId) throws NoTrobatException;
+	public void comprovarEstatNotificacions() throws NoTrobatException;
+	public void actualitzarEstatNotificacions(Long notificacioId) throws NoTrobatException;
+	
 }
