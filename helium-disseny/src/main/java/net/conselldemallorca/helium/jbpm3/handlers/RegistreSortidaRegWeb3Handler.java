ef89ef12ba69511f250d6aaa2add831df9d6e2d6
Josep Gayà <josepg@limit.es>
2017-07-24 15:36:20 +0200

    Actualització llibreria disseny

--- helium-disseny/src/main/java/net/conselldemallorca/helium/jbpm3/handlers/RegistreSortidaRegWeb3Handler.java
+++ helium-disseny/src/main/java/net/conselldemallorca/helium/jbpm3/handlers/RegistreSortidaRegWeb3Handler.java
@@ -16,5 +16,75 @@ public class RegistreSortidaRegWeb3Handler extends BasicActionHandler {
 	public RegistreSortidaRegWeb3Handler() {}
 	
 	public void execute(ExecutionContext executionContext) throws Exception {}
+	
+	public void setOrganOrigen(String organOrigen) {}
+	public void setVarOrganOrigen(String varOrganOrigen) {}
+	public void setOficinaCodi(String oficinaCodi) {}
+	public void setVarOficinaCodi(String varOficinaCodi) {}
+	public void setLlibreCodi(String llibreCodi) {}
+	public void setVarLlibreCodi(String varLlibreCodi) {}
+	public void setExtracte(String extracte) {}
+	public void setVarExtracte(String varExtracte) {}
+	public void setDocumentacioFisicaCodi(String documentacioFisicaCodi) {}
+	public void setVarDocumentacioFisicaCodi(String varDocumentacioFisicaCodi) {}
+	public void setAssumpteTipusCodi(String assumpteTipusCodi) {}
+	public void setVarAssumpteTipusCodi(String varAssumpteTipusCodi) {}
+	public void setAssumpteCodi(String assumpteCodi) {}
+	public void setVarAssumpteCodi(String varAssumpteCodi) {}
+	public void setInteressatTipus(String interessatTipus) {}
+	public void setVarInteressatTipus(String varInteressatTipus) {}
+	public void setInteressatDocumentTipus(String interessatDocumentTipus) {}
+	public void setVarInteressatDocumentTipus(String varInteressatDocumentTipus) {}
+	public void setInteressatDocumentNum(String interessatDocumentNum) {}
+	public void setVarInteressatDocumentNum(String varInteressatDocumentNum) {}
+	public void setInteressatEmail(String interessatEmail) {}
+	public void setVarInteressatEmail(String varInteressatEmail) {}
+	public void setInteressatTelefon(String interessatTelefon) {}
+	public void setVarInteressatTelefon(String varInteressatTelefon) {}
+	public void setInteressatNom(String interessatNom) {}
+	public void setVarInteressatNom(String varInteressatNom) {}
+	public void setInteressatLlinatge1(String interessatLlinatge1) {}
+	public void setVarInteressatLlinatge1(String varInteressatLlinatge1) {}
+	public void setInteressatLlinatge2(String interessatLlinatge2) {}
+	public void setVarInteressatLlinatge2(String varInteressatLlinatge2) {}
+	public void setInteressatPais(String interessatPais) {}
+	public void setVarInteressatPais(String varInteressatPais) {}
+	public void setInteressatProvincia(String interessatProvincia) {}
+	public void setVarInteressatProvincia(String varInteressatProvincia) {}
+	public void setInteressatMunicipi(String interessatMunicipi) {}
+	public void setVarInteressatMunicipi(String varInteressatMunicipi) {}
+	public void setInteressatCodiPostal(String interessatCodiPostal) {}
+	public void setVarInteressatCodiPostal(String varInteressatCodiPostal) {}
+	public void setInteressatAdresa(String interessatAdresa) {}
+	public void setVarInteressatAdresa(String varInteressatAdresa) {}
+	public void setRepresentantTipus(String representantTipus) {}
+	public void setVarRepresentantTipus(String varRepresentantTipus) {}
+	public void setRepresentantDocumentTipus(String representantDocumentTipus) {}
+	public void setVarRepresentantDocumentTipus(String varRepresentantDocumentTipus) {}
+	public void setRepresentantDocumentNum(String representantDocumentNum) {}
+	public void setVarRepresentantDocumentNum(String varRepresentantDocumentNum) {}
+	public void setRepresentantEmail(String representantEmail) {}
+	public void setVarRepresentantEmail(String varRepresentantEmail) {}
+	public void setRepresentantTelefon(String representantTelefon) {}
+	public void setVarRepresentantTelefon(String varRepresentantTelefon) {}
+	public void setRepresentantNom(String representantNom) {}
+	public void setVarRepresentantNom(String varRepresentantNom) {}
+	public void setRepresentantLlinatge1(String representantLlinatge1) {}
+	public void setVarRepresentantLlinatge1(String varRepresentantLlinatge1) {}
+	public void setRepresentantLlinatge2(String representantLlinatge2) {}
+	public void setVarRepresentantLlinatge2(String varRepresentantLlinatge2) {}
+	public void setRepresentantPais(String representantPais) {}
+	public void setVarRepresentantPais(String varRepresentantPais) {}
+	public void setRepresentantProvincia(String representantProvincia) {}
+	public void setVarRepresentantProvincia(String varRepresentantProvincia) {}
+	public void setRepresentantMunicipi(String representantMunicipi) {}
+	public void setVarRepresentantMunicipi(String varRepresentantMunicipi) {}
+	public void setRepresentantCodiPostal(String representantCodiPostal) {}
+	public void setVarRepresentantCodiPostal(String varRepresentantCodiPostal) {}
+	public void setRepresentantAdresa(String representantAdresa) {}
+	public void setVarRepresentantAdresa(String varRepresentantAdresa) {}
+	public void setIdiomaCodi(String idiomaCodi) {}
+	public void setVarIdiomaCodi(String varIdiomaCodi) {}
+	public void setVarDocument(String varDocument) {}
 
 }
