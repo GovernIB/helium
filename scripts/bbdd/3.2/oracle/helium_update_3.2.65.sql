24acb7be67bbd0273637d47da7e43f56b2eb9125
Josep Gayà <josepg@limit.es>
2017-09-19 11:28:50 +0200

    #1082: Generació de les metadades NTI
    Task-Url: https://github.com/GovernIB/helium/issues/issues/1082
    
    Rectificació scripts BBDD
    
    Conflicts:
    	scripts/bbdd/3.2/oracle/helium_update_3.2.65.sql
    	scripts/bbdd/3.2/postgresql/helium_update_3.2.65.sql

--- scripts/bbdd/3.2/oracle/helium_update_3.2.65.sql
+++ scripts/bbdd/3.2/oracle/helium_update_3.2.65.sql
@@ -1,4 +1,17 @@
 -------------------------------------------------------------------
+-- #1098 Error esborrant versions de definicions de procés
+-- Modifica les constraints de claus foràrines de jbpm per esborrar en cascada
+-------------------------------------------------------------------
+
+ALTER TABLE JBPM_SWIMLANE DROP CONSTRAINT FK_SWL_ASSDEL;
+
+ALTER TABLE JBPM_SWIMLANE ADD
+  CONSTRAINT FK_SWL_ASSDEL 
+ FOREIGN KEY (ASSIGNMENTDELEGATION_) 
+ REFERENCES JBPM_DELEGATION (ID_)
+ ON DELETE CASCADE;
+
+-------------------------------------------------------------------
 -- Metadades Nti
 -------------------------------------------------------------------
 ALTER TABLE HELIUM.HEL_DOCUMENT ADD (NTI_TIPO_DOC VARCHAR2(32 CHAR));
