34908c04e9eecd9808b3ce54b6eb22b77ccbe475
Josep Gayà <josepg@limit.es>
2017-09-19 14:54:29 +0200

    #1082: Generació de les metadades NTI
    Task-Url: https://github.com/GovernIB/helium/issues/issues/1082
    
    Correcció validació a l'hora de crear o modificar document i nullpointer
    a l'hora de generar si la propietat nti no està informada.

--- helium-webapp/src/main/java/net/conselldemallorca/helium/webapp/v3/validator/ExpedientTipusDocumentValidator.java
+++ helium-webapp/src/main/java/net/conselldemallorca/helium/webapp/v3/validator/ExpedientTipusDocumentValidator.java
@@ -1,14 +1,21 @@
 package net.conselldemallorca.helium.webapp.v3.validator;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import net.conselldemallorca.helium.v3.core.api.dto.DefinicioProcesDto;
 import net.conselldemallorca.helium.v3.core.api.dto.DocumentDto;
+import net.conselldemallorca.helium.v3.core.api.dto.EntornDto;
+import net.conselldemallorca.helium.v3.core.api.dto.ExpedientTipusDto;
+import net.conselldemallorca.helium.v3.core.api.service.DefinicioProcesService;
 import net.conselldemallorca.helium.v3.core.api.service.DocumentService;
+import net.conselldemallorca.helium.v3.core.api.service.ExpedientTipusService;
 import net.conselldemallorca.helium.webapp.v3.command.ExpedientTipusDocumentCommand;
 import net.conselldemallorca.helium.webapp.v3.helper.MessageHelper;
+import net.conselldemallorca.helium.webapp.v3.helper.SessionHelper;
 
 /**
  * Validador per al manteniment de variables del tipus d'expedient:
@@ -22,8 +29,15 @@ import net.conselldemallorca.helium.webapp.v3.helper.MessageHelper;
 public class ExpedientTipusDocumentValidator implements ConstraintValidator<ExpedientTipusDocument, ExpedientTipusDocumentCommand>{
 
 	private String codiMissatge;
+
 	@Autowired
 	DocumentService documentService;
+	@Autowired
+	ExpedientTipusService expedientTipusService;
+	@Autowired
+	DefinicioProcesService definicioProcesService;
+	@Autowired
+	private HttpServletRequest request;
 
 	@Override
 	public void initialize(ExpedientTipusDocument anotacio) {
@@ -47,6 +61,36 @@ public class ExpedientTipusDocumentValidator implements ConstraintValidator<Expe
 				valid = false;
 			}
 		}
+		// Comprova les dades NTI si el tipus d'expedient està configurat com a tal
+		EntornDto entornActual = SessionHelper.getSessionManager(request).getEntornActual();
+		Long expedientTipusId = null;
+		if (document.getExpedientTipusId() != null) {
+			expedientTipusId = document.getExpedientTipusId();
+		} else {
+			// Prova si la definició de procés no és null per obtenir l'expedient tipus id
+			if (document.getDefinicioProcesId() != null){
+				DefinicioProcesDto definicioProces = definicioProcesService.findAmbIdAndEntorn(
+						entornActual.getId(), 
+						document.getDefinicioProcesId());
+				if (definicioProces != null && definicioProces.getExpedientTipus() != null)
+					expedientTipusId = definicioProces.getExpedientTipus().getId();
+			}
+		}
+		if (expedientTipusId != null) {
+			ExpedientTipusDto expedientTipus = expedientTipusService.findAmbIdPermisConsultar(
+					entornActual.getId(), 
+					expedientTipusId);
+			if (expedientTipus.isNtiActiu() 
+					&& (document.getNtiTipusDocumental() == null || "".equals(document.getNtiTipusDocumental().trim()))) {
+				context.buildConstraintViolationWithTemplate(
+						MessageHelper.getInstance().getMessage("NotEmpty", null))
+						.addNode("ntiTipusDocumental")
+						.addConstraintViolation();	
+				valid = false;
+			}
+		}
+		if (!valid)
+			context.disableDefaultConstraintViolation();
 
 		return valid;
 	}
