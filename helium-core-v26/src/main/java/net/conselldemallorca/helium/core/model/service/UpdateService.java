d64679905291dcd5715702b94aad1a06fce2b1c8
Josep Gayà <josepg@limit.es>
2019-04-12 11:40:57 +0200

    Tancament versió 3.2.105

--- helium-core-v26/src/main/java/net/conselldemallorca/helium/core/model/service/UpdateService.java
+++ helium-core-v26/src/main/java/net/conselldemallorca/helium/core/model/service/UpdateService.java
@@ -61,7 +61,7 @@ public class UpdateService {
 	public static final String VERSIO_ACTUAL_STR = "3.2.0";
 	public static final int VERSIO_ACTUAL_ORDRE = 320;
 
-	public static final String VERSIO_ACTUAL_RELEASE = "104.2";
+	public static final String VERSIO_ACTUAL_RELEASE = "105";
 
 	private VersioDao versioDao;
 	private PersonaDao personaDao;
