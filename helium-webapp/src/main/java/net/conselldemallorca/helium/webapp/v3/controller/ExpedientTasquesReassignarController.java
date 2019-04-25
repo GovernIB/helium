1a4e1a057c2f7996a03fc751aa06aae5b956c2ae
Josep Gayà <josepg@limit.es>
2019-04-25 14:02:33 +0200

    #1297: Millora modal reassignació d'una tasca
    Task-Url: #1297

--- helium-webapp/src/main/java/net/conselldemallorca/helium/webapp/v3/controller/ExpedientTasquesReassignarController.java
+++ helium-webapp/src/main/java/net/conselldemallorca/helium/webapp/v3/controller/ExpedientTasquesReassignarController.java
@@ -3,26 +3,31 @@
  */
 package net.conselldemallorca.helium.webapp.v3.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.support.SessionStatus;
 
-import net.conselldemallorca.helium.v3.core.api.dto.ExpedientTascaDto;
+import net.conselldemallorca.helium.v3.core.api.dto.PersonaDto;
+import net.conselldemallorca.helium.v3.core.api.service.AplicacioService;
 import net.conselldemallorca.helium.v3.core.api.service.ExpedientTascaService;
-import net.conselldemallorca.helium.v3.core.api.service.TascaService;
 import net.conselldemallorca.helium.webapp.v3.command.ExpedientTascaReassignarCommand;
 import net.conselldemallorca.helium.webapp.v3.helper.MissatgesHelper;
 
@@ -36,9 +41,9 @@ import net.conselldemallorca.helium.webapp.v3.helper.MissatgesHelper;
 public class ExpedientTasquesReassignarController extends BaseExpedientController {
 
 	@Autowired
-	private TascaService tascaService;
-	@Autowired
 	private ExpedientTascaService expedientTascaService;
+	@Autowired
+	private AplicacioService aplicacioService;
 
 
 
@@ -47,11 +52,9 @@ public class ExpedientTasquesReassignarController extends BaseExpedientControlle
 			HttpServletRequest request,
 			@PathVariable Long expedientId,
 			@PathVariable String tascaId,
-			ModelMap model) {		
-		atributsModel(
-    			expedientId,
-    			tascaId,
-    			model);
+			ModelMap model) {
+		ExpedientTascaReassignarCommand expedientTascaReassignarCommand = new ExpedientTascaReassignarCommand();
+		model.addAttribute(expedientTascaReassignarCommand);
 		return "v3/expedient/tasca/reassignar";
 	}
 	
@@ -61,33 +64,32 @@ public class ExpedientTasquesReassignarController extends BaseExpedientControlle
 			@PathVariable Long expedientId,
 			@PathVariable String tascaId,
 			@RequestParam(value = "submit", required = false) String submit,
-			ExpedientTascaReassignarCommand expedientTascaReassignarCommand,
+			@ModelAttribute("expedientTascaReassignarCommand") ExpedientTascaReassignarCommand expedientTascaReassignarCommand,
 			BindingResult result,
 			SessionStatus status,
 			ModelMap model) {
+		String tipus = request.getParameter("tipusExpressio"); 
 		if ("submit".equals(submit) || submit.length() == 0) {
-			new TascaReassignarValidator().validate(expedientTascaReassignarCommand, result);
+			new TascaReassignarValidator().setTipus(tipus).validate(expedientTascaReassignarCommand, result);
 	        if (result.hasErrors()) {
-	        	atributsModel(
-	        			expedientId,
-	        			tascaId,
-	        			model);
 				MissatgesHelper.error(request, result, getMessage(request, "error.validacio"));
 	        	return "v3/expedient/tasca/reassignar";
 	        }
 			try {
+				String expression = expedientTascaReassignarCommand.getExpression();
+				if ("user".equals(tipus)) {
+					expression = "user(" + expedientTascaReassignarCommand.getUsuari() + ")";
+				} else if ("grup".equals(tipus)) {
+					expression = "group(" + expedientTascaReassignarCommand.getGrup() + ")";
+				}
 				expedientTascaService.reassignar(
 						expedientId,
-						expedientTascaReassignarCommand.getTaskId(),
-						expedientTascaReassignarCommand.getExpression());
+						tascaId,
+						expression);
 				MissatgesHelper.success(request, getMessage(request, "info.tasca.reassignada"));
 			} catch (Exception ex) {
-				MissatgesHelper.error(request, getMessage(request, "error.reassignar.tasca", new Object[] { expedientTascaReassignarCommand.getTaskId() } ));
-				atributsModel(
-	        			expedientId,
-	        			tascaId,
-	        			model);
-	        	logger.error("No s'ha pogut reassignar la tasca " + expedientTascaReassignarCommand.getTaskId(), ex);
+				MissatgesHelper.error(request, getMessage(request, "error.reassignar.tasca", new Object[] { tascaId } ));
+	        	logger.error("No s'ha pogut reassignar la tasca " + tascaId, ex);
 	        	return "v3/expedient/tasca/reassignar";
 			}
 		}
@@ -95,29 +97,54 @@ public class ExpedientTasquesReassignarController extends BaseExpedientControlle
 		return modalUrlTancar(false);
 	}
 	
+	@RequestMapping(value = "/persona/suggest/{text}", method = RequestMethod.GET, produces={"application/json; charset=UTF-8"})
+	@ResponseBody
+	public String personaSuggest(
+			@PathVariable String text,
+			Model model) {
+		List<PersonaDto> lista = aplicacioService.findPersonaLikeNomSencer(text);
+		String json = "[";
+		for (PersonaDto persona: lista) {
+			json += "{\"codi\":\"" + persona.getCodi() + "\", \"nom\":\"" + persona.getNomSencer() + "\"},";
+		}
+		if (json.length() > 1) json = json.substring(0, json.length() - 1);
+		json += "]";
+		return json;
+	}
+
+	@RequestMapping(value = "/persona/suggestInici/{text}", method = RequestMethod.GET, produces={"application/json; charset=UTF-8"})
+	@ResponseBody
+	public String personaSuggestInici(
+			@PathVariable String text,
+			Model model) {
+		PersonaDto persona = aplicacioService.findPersonaAmbCodi(text);
+		if (persona != null) {
+			return "{\"codi\":\"" + persona.getCodi() + "\", \"nom\":\"" + persona.getNomSencer() + "\"}";
+		}
+		return null;
+	}
+	
 	private class TascaReassignarValidator implements Validator {
+		private String tipus;
+		
 		@SuppressWarnings({ "unchecked", "rawtypes" })
 		public boolean supports(Class clazz) {
 			return clazz.isAssignableFrom(ExpedientTascaReassignarCommand.class);
 		}
 		public void validate(Object target, Errors errors) {
-			ValidationUtils.rejectIfEmpty(errors, "expression", "not.blank");
+			if ("user".equals(tipus)) {
+				ValidationUtils.rejectIfEmpty(errors, "usuari", "not.blank");
+			} else if ("grup".equals(tipus)) {
+				ValidationUtils.rejectIfEmpty(errors, "grup", "not.blank");
+			} else if ("expr".equals(tipus)) {
+				ValidationUtils.rejectIfEmpty(errors, "expression", "not.blank");
+			}
 		}
-	}
-	
-	private void atributsModel(
-			Long expedientId,
-			String tascaId,
-			ModelMap model) {
-		ExpedientTascaDto tasca = tascaService.findAmbIdPerExpedient(
-				tascaId,
-				expedientId);
+		public TascaReassignarValidator setTipus(String tipus) {
+			this.tipus = tipus;
+			return this;
+		}
 		
-		ExpedientTascaReassignarCommand expedientTascaReassignarCommand = new ExpedientTascaReassignarCommand();
-		expedientTascaReassignarCommand.setTaskId(tascaId);
-		
-		model.addAttribute(expedientTascaReassignarCommand);
-		model.addAttribute("expedientIdentificador",tasca.getExpedientIdentificador());
 	}
 
 	private static final Log logger = LogFactory.getLog(ExpedientTasquesReassignarController.class);
--- helium-webapp/src/main/webapp/WEB-INF/jsp/v3/expedient/tasca/reassignar.jsp
+++ helium-webapp/src/main/webapp/WEB-INF/jsp/v3/expedient/tasca/reassignar.jsp
@@ -3,24 +3,121 @@
 <%@ taglib uri="http://www.springframework.org/tags" prefix="spring"%>
 <%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
 <%@ taglib tagdir="/WEB-INF/tags/helium" prefix="hel"%>
+<c:set var="idioma"><%=org.springframework.web.servlet.support.RequestContextUtils.getLocale(request).getLanguage()%></c:set>
 
 <html>
 <head>
 	<title><spring:message code='alerta.llistat.expedient' />: ${expedientIdentificador}</title>
 	<meta name="capsaleraTipus" content="llistat"/>
-	<link href="<c:url value="/css/tabs.css"/>" rel="stylesheet" type="text/css"/>
-	<link href="<c:url value="/css/displaytag.css"/>" rel="stylesheet" type="text/css"/>
 	<hel:modalHead/>
-	<style>
-		body {background-image: none;}
-		.col-xs-4 {width: auto;}
-		.col-xs-8 {width: 77%;}
-		.form-group {width: 100%;}
-	</style>
+	<link href="<c:url value="/css/select2.css"/>" rel="stylesheet"/>
+		<link href="<c:url value="/css/select2-bootstrap.css"/>" rel="stylesheet"/>
+		<link href="<c:url value="/css/DT_bootstrap.css"/>" rel="stylesheet">
+	
+		<script src="<c:url value="/js/jquery/jquery.maskedinput.js"/>"></script>
+		<script src="<c:url value="/js/jquery.dataTables.js"/>"></script>
+		<script src="<c:url value="/js/DT_bootstrap.js"/>"></script>
+		<script src="<c:url value="/js/jsrender.min.js"/>"></script>
+		<script src="<c:url value="/js/helium.datatable.js"/>"></script>
+		<script src="<c:url value="/js/select2.min.js"/>"></script>
+		<script src="<c:url value="/js/select2-locales/select2_locale_${idioma}.js"/>"></script>
+		
+		<script src="<c:url value="/js/moment.js"/>"></script>
+		<script src="<c:url value="/js/moment-with-locales.min.js"/>"></script>
+		<script src="<c:url value="/js/bootstrap-datetimepicker.js"/>"></script>
+		<link href="<c:url value="/css/bootstrap-datetimepicker.min.css"/>" rel="stylesheet">
+		
+		<style>
+			body {background-image: none; padding-top: 0px;}
+			.col-xs-4 {width: auto;padding-left: 0px;}
+			.col-xs-8 {width: 100%;padding-bottom: 100px;padding-left: 0px;padding-right: 0px;}
+			#opciones .label-titol {padding-bottom: 0px;} 
+	 		.control-group {width: 100%;display: inline-block;} 
+	 		.control-group-mid {width: 48%;} 
+	  		.control-group.left {float: left; margin-right:1%;} 
+	  		.ctrlHolder {padding-top: 10px;}
+	  		.select2-drop {left: 15px !important;right: 15px !important;}
+	  		#div_timer label {float:left;} 
+			.label-titol {background-color: #fefefe; border: 1px solid #e3e3e3; border-radius: 4px; box-shadow: 0 1px 1px rgba(0, 0, 0, 0.05) inset; margin-bottom: 20px; min-height: 20px; padding: 19px;}			
+			.label-titol .control-label {padding-bottom: 20px;}		
+			.label-titol .form-group .control-label{padding-bottom: 0px;}	
+			.obligatori {
+			    background-position: right 6px;
+			}
+		</style>
+	<script>
+			$(document).ready(function() {
+				$('.date_time').datetimepicker({
+					locale: moment.locale('${idioma}'),
+					minDate: new Date(),
+					format: "DD/MM/YYYY HH:mm"
+			    });	
+				
+				$("#tipusExpressio").select2({
+				    width: '100%',
+				    minimumResultsForSearch: -1,
+				    allowClear: true
+				});
+				
+				$("#inici").on('focus', function() {
+					$('.fa-calendar').click();
+				});	
+				
+				$('#tipusExpressio').on('change', function() {
+					$("#duser").hide();
+					$("#dgrup").hide();
+					$("#dexpr").hide();
+					if ("user" == this.value) {
+						$("#duser").show();
+					} else if ("grup" == this.value) {
+						$("#dgrup").show();
+					} else if ("expr" == this.value) {
+						$("#dexpr").show();
+					}
+				});
+				$('#tipusExpressio').trigger('change');	
+			});
+			
+			function confirmarReassignar(e) {
+				var e = e || window.event;
+				e.cancelBubble = true;
+				if (e.stopPropagation) e.stopPropagation();
+				return confirm("<spring:message code='expedient.eines.confirm_reassignar_expedients' />");
+			}
+		</script>
 </head>
 <body>
-	<form:form action="reassignar" cssClass="form-horizontal form-tasca" commandName="expedientTascaReassignarCommand">
-		<form:hidden path="taskId"/>
+	<form:form action="reassignar" method="post" commandName="expedientTascaReassignarCommand" onsubmit="return confirmarReassignar(event)">
+		
+		<div class="ctrlHolder">
+			<label for="tipusExpressio"><spring:message code='expedient.eines.reassignar.tipus'/></label>
+			<select id="tipusExpressio" name="tipusExpressio">
+				<option value="user"><spring:message code='filtre.expressio.usuari'/></option>
+				<option value="grup"><spring:message code='filtre.expressio.grup'/></option>
+				<option value="expr"><spring:message code='filtre.expressio.expressio'/></option>
+			</select>
+		</div>
+		
+		<div class="ctrlHolder">
+			<div id="duser">
+				<hel:inputSuggest required="true" name="usuari" urlConsultaInicial="persona/suggestInici" urlConsultaLlistat="/helium/v3/expedient/persona/suggest" textKey="expedient.editar.responsable" placeholderKey="expedient.editar.responsable"/>
+			</div>
+			<div id="dgrup">
+				<hel:inputText required="true" name="grup" textKey="filtre.expressio.grup" placeholderKey="filtre.expressio.grup"/>
+			</div>
+			<div id="dexpr">
+				<hel:inputText required="true" name="expression" textKey="filtre.expressio.expressio" placeholderKey="filtre.expressio.expressio"/>
+			</div>
+		</div>				
+				
+		<div id="modal-botons">
+			<button type="button" class="modal-tancar btn btn-default" name="submit" value="cancel">
+				<spring:message code='comuns.cancelar' />
+			</button>
+			<button type="submit" name="submit" value="submit" class="btn btn-primary"><span class="fa fa-share-square-o"></span>&nbsp;<spring:message code="comuns.reassignar"/></button>
+		</div>
+	
+		<!-- form:hidden path="taskId"/>
 
 		<div class="control-group fila_reducida">
 			<hel:inputText required="true" name="expression" textKey="expedient.tasca.expresio_assignacio" placeholderKey="expedient.tasca.expresio_assignacio"/>
@@ -29,7 +126,7 @@
 		<div id="modal-botons">
 			<button type="button" class="modal-tancar btn btn-default" name="submit" value="cancel"><spring:message code="comuns.cancelar"/></button>
 			<button type="submit" class="btn btn-primary" id="submit" name="submit" value="submit"><span class="fa fa-share-square-o"></span>&nbsp;<spring:message code="comuns.reassignar"/></button>
-		</div>
+		</div-->
 	</form:form>
 </body>
 </html>
