867b2c238fef1c93f4f98f89302f84515413134b
Josep Gayà <josepg@limit.es>
2020-09-24 13:39:32 +0200

    #1383 Creació d'índexs per millorar rendiment consultes
    Task-url: https://github.com/GovernIB/helium/issues/1383

--- helium-core-v26/src/main/java/net/conselldemallorca/helium/core/model/service/UpdateService.java
+++ helium-core-v26/src/main/java/net/conselldemallorca/helium/core/model/service/UpdateService.java
@@ -61,8 +61,8 @@ public class UpdateService {
 	public static final String VERSIO_ACTUAL_STR = "3.2.0";
 	public static final int VERSIO_ACTUAL_ORDRE = 320;
 
-	public static final String VERSIO_ACTUAL_RELEASE = "109";
-	public static final String VERSIO_ACTUAL_DATA = "2020.09.10 12:00";
+	public static final String VERSIO_ACTUAL_RELEASE = "110";
+	public static final String VERSIO_ACTUAL_DATA = "2020.09.24 12:00";
 
 	private VersioDao versioDao;
 	private PersonaDao personaDao;
