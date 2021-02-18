029ab985f347ef1de8d66a63c6276aaea87dd8de
josepg-limit <josepg@limit.es>
2021-02-18 12:05:10 +0100

    Definir Spring cloud config server

---
+++ microservices/heliium-config-server/src/test/java/es/caib/helium/configserver/HeliiumConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package es.caib.helium.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeliiumConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
