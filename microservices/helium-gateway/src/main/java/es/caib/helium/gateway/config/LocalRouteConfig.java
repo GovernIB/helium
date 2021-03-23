a2f173d1e30055a6d51de519c135828ee074e34e
josepg-limit <josepg@limit.es>
2021-03-23 08:41:11 +0100

    Configuració de logstash i compose

--- microservices/helium-gateway/src/main/java/es/caib/helium/gateway/config/LocalRouteConfig.java
+++ microservices/helium-gateway/src/main/java/es/caib/helium/gateway/config/LocalRouteConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-@Profile("!spring-cloud")
+@Profile("!spring-cloud && !compose")
 @Configuration
 public class LocalRouteConfig {
 
