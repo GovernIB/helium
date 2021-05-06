24a8fc4e72e2bc27ae07a7600d49193bdffd2319
josepg-limit <josepg@limit.es>
2021-05-06 10:45:55 +0200

    #1500 Helium MS. Fase 1 Crear microservei de serveis externs

---
+++ microservices/helium-integracio-service/src/test/java/es/caib/helium/integracio/HeliumIntegracioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package es.caib.helium.integracio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeliumIntegracioServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
