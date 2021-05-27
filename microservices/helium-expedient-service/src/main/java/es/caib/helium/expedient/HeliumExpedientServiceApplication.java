1fe164ad3a206e1658cef7bd57bb8a667ba4a791
josepg-limit <josepg@limit.es>
2021-05-27 12:37:25 +0200

    #1505 Helium MS. Fase 1 Desenvolupar la API REST expedients i tasques
    - Creació del projecte en blanc

---
+++ microservices/helium-expedient-service/src/main/java/es/caib/helium/expedient/HeliumExpedientServiceApplication.java
@@ -0,0 +1,13 @@
+package es.caib.helium.expedient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeliumExpedientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeliumExpedientServiceApplication.class, args);
+	}
+
+}
