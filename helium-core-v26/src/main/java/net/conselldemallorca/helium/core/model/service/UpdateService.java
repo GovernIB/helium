f66050f5e1e3676112900bb1be6e1016d7302215
josepg-limit <josepg@limit.es>
2021-09-22 13:49:15 +0200

    Tancament Helium 3.2.113 (2021.09.22 12:00)

--- helium-core-v26/src/main/java/net/conselldemallorca/helium/core/model/service/UpdateService.java
+++ helium-core-v26/src/main/java/net/conselldemallorca/helium/core/model/service/UpdateService.java
@@ -61,8 +61,8 @@ public class UpdateService {
 	public static final String VERSIO_ACTUAL_STR = "3.2.0";
 	public static final int VERSIO_ACTUAL_ORDRE = 320;
 
-	public static final String VERSIO_ACTUAL_RELEASE = "112.3";
-	public static final String VERSIO_ACTUAL_DATA = "2021.07.01 11:00";
+	public static final String VERSIO_ACTUAL_RELEASE = "113";
+	public static final String VERSIO_ACTUAL_DATA = "2021.09.22 12:00";
 
 	private VersioDao versioDao;
 	private PersonaDao personaDao;
