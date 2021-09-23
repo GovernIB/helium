4833fc1c7fbc915266d9f07b1dda95464c3cc406
josepg-limit <josepg@limit.es>
2021-09-23 11:10:11 +0200

    Tancament Helium 3.2.113 (2021.09.23 11:00h)
    - Correcció camps compostos Sistra2

--- helium-core-v26/src/main/java/net/conselldemallorca/helium/core/model/service/UpdateService.java
+++ helium-core-v26/src/main/java/net/conselldemallorca/helium/core/model/service/UpdateService.java
@@ -62,7 +62,7 @@ public class UpdateService {
 	public static final int VERSIO_ACTUAL_ORDRE = 320;
 
 	public static final String VERSIO_ACTUAL_RELEASE = "113";
-	public static final String VERSIO_ACTUAL_DATA = "2021.09.22 12:00";
+	public static final String VERSIO_ACTUAL_DATA = "2021.09.23 11:00";
 
 	private VersioDao versioDao;
 	private PersonaDao personaDao;
