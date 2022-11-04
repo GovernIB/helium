453ac17b351eb467ccfa9dc44ff87cfadd56c787
josepg-limit <josepg@limit.es>
2022-11-04 11:01:49 +0100

    Correcció i tancament d' Helium 3.2.115.1 (2022.11.04 10:00)
    - Correcció del botó de logout

--- helium-core-v26/src/main/java/net/conselldemallorca/helium/core/model/service/UpdateService.java
+++ helium-core-v26/src/main/java/net/conselldemallorca/helium/core/model/service/UpdateService.java
@@ -62,7 +62,7 @@ public class UpdateService {
 	public static final int VERSIO_ACTUAL_ORDRE = 320;
 
 	public static final String VERSIO_ACTUAL_RELEASE = "115.1";
-	public static final String VERSIO_ACTUAL_DATA = "2022.10.28 12:00";
+	public static final String VERSIO_ACTUAL_DATA = "2022.11.04 10:00";
 
 	private VersioDao versioDao;
 	private PersonaDao personaDao;
