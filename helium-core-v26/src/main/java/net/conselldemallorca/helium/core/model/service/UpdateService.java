86884c4cc70f17661b31f036fabfd4cc9897a7a2
josepg-limit <josepg@limit.es>
2023-01-02 12:56:26 +0100

    #1641 Escurçar llargada nom unitat tramitadora per validació PINBAL

--- helium-core-v26/src/main/java/net/conselldemallorca/helium/core/model/service/UpdateService.java
+++ helium-core-v26/src/main/java/net/conselldemallorca/helium/core/model/service/UpdateService.java
@@ -62,7 +62,7 @@ public class UpdateService {
 	public static final int VERSIO_ACTUAL_ORDRE = 320;
 
 	public static final String VERSIO_ACTUAL_RELEASE = "115.1.2";
-	public static final String VERSIO_ACTUAL_DATA = "2023.01.02 09:00";
+	public static final String VERSIO_ACTUAL_DATA = "2023.01.02 13:00";
 
 	private VersioDao versioDao;
 	private PersonaDao personaDao;
