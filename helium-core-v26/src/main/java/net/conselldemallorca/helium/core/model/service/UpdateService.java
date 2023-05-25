2fc2ddfa68827039a61b34a19ec54c79b8650637
Josep Gayà <josepg@limit.es>
2023-05-25 16:49:00 +0200

    Correccions del darrer merge

--- helium-core-v26/src/main/java/net/conselldemallorca/helium/core/model/service/UpdateService.java
+++ helium-core-v26/src/main/java/net/conselldemallorca/helium/core/model/service/UpdateService.java
@@ -62,7 +62,7 @@ public class UpdateService {
 	public static final int VERSIO_ACTUAL_ORDRE = 320;
 
 	public static final String VERSIO_ACTUAL_RELEASE = "116_Helium_SW";
-	public static final String VERSIO_ACTUAL_DATA = "2023.06.18 09:00";
+	public static final String VERSIO_ACTUAL_DATA = "2023.05.25 15:00";
 
 	private VersioDao versioDao;
 	private PersonaDao personaDao;
