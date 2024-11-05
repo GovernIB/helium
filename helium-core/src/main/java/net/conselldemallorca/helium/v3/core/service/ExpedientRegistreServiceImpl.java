a2bf9fb003c2f359dea1035c06b0ed94633290a0
Josep Gayà <josepg@limit.es>
2024-11-05 19:13:15 +0100

    #1803 Error 500 accedint pipella registre dins gestió expedient

--- helium-core/src/main/java/net/conselldemallorca/helium/v3/core/service/ExpedientRegistreServiceImpl.java
+++ helium-core/src/main/java/net/conselldemallorca/helium/v3/core/service/ExpedientRegistreServiceImpl.java
@@ -175,7 +175,7 @@ public class ExpedientRegistreServiceImpl implements ExpedientRegistreService {
 							tascaHelper.toExpedientTascaDto(
 									task,
 									expedient,
-									true,
+									false,
 									false));
 				}
 			}
