bf94124948e638b26a3322ed4cd26857379c64ba
Josep Gayà <josepg@limit.es>
2024-12-10 12:42:27 +0100

    Tancament Helium 3.3.6 (2024.12.04 17:00h)

--- versions/3.3.6/1798.sql
+++ versions/3.3.6/1798.sql
@@ -1,7 +1,7 @@
--- #1795 Avís per email de nova anotació rebuda a Helium
+-- #1798 Resoldre el nom de la via pels interessats de tipus administració
 
 -- Oracle
-ALTER TABLE HEL_UNITAT_ORGANITZATIVA MODIFY (ADRESSA VARCHAR(255 CHAR));
+ALTER TABLE HEL_UNITAT_ORGANITZATIVA MODIFY (ADRESSA VARCHAR2(255 CHAR));
 
 
     
