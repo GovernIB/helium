a737b711c930406a8215124047b6e3a6961a58fb
Josep Gayà <josepg@limit.es>
2024-12-12 13:30:48 +0100

    Tancament Helium 3.3.6 (2024.12.04 17:00h)
    - Completar i corregir scripts.

--- versions/3.3.6/1798.sql
+++ versions/3.3.6/1798.sql
@@ -3,7 +3,16 @@
 -- Oracle
 ALTER TABLE HEL_UNITAT_ORGANITZATIVA MODIFY (ADRESSA VARCHAR2(255 CHAR));
 
+-- Posar dates de sincronització a null per tornar a recuperar l'adreça de totes les unitats organitzatives
+UPDATE HEL_PARAMETRE SET VALOR = NULL WHERE CODI LIKE 'app.net.caib.helium.unitats.organitzatives.data.sincronitzacio';
+UPDATE HEL_PARAMETRE SET VALOR = NULL WHERE CODI LIKE 'app.net.caib.helium.unitats.organitzatives.data.actualitzacio';
+
+
 
     
 -- Postgresql
 ALTER TABLE HEL_UNITAT_ORGANITZATIVA MODIFY (ADRESSA VARCHAR(255));
+
+-- Posar dates de sincronització a null per tornar a recuperar l'adreça de totes les unitats organitzatives
+UPDATE HEL_PARAMETRE SET VALOR = NULL WHERE CODI LIKE 'app.net.caib.helium.unitats.organitzatives.data.sincronitzacio';
+UPDATE HEL_PARAMETRE SET VALOR = NULL WHERE CODI LIKE 'app.net.caib.helium.unitats.organitzatives.data.actualitzacio';
