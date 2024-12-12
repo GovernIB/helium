b37445838c939a853a861e96657bcfff54698351
Josep Gayà <josepg@limit.es>
2024-12-12 17:21:12 +0100

    Tancament Helium 3.3.6.1 (2024.12.12 17:00h)

--- helium-core-v26/src/main/java/net/conselldemallorca/helium/core/model/service/UpdateService.java
+++ helium-core-v26/src/main/java/net/conselldemallorca/helium/core/model/service/UpdateService.java
@@ -64,7 +64,7 @@ public class UpdateService {
 	public static final int VERSIO_ACTUAL_ORDRE = 330;
 
 	public static final String VERSIO_ACTUAL_RELEASE = "6.1";
-	public static final String VERSIO_ACTUAL_DATA = "2024.12.11 09:00h";
+	public static final String VERSIO_ACTUAL_DATA = "2024.12.12 17:00h";
 
 	private VersioDao versioDao;
 	private PersonaDao personaDao;
