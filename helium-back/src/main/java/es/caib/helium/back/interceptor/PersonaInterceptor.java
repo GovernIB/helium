21d3a1e8af52f39819c2955736f75ced54a07b1f
Josep Gayà <josepg@limit.es>
2026-06-23 16:42:28 +0200

    Proves de regressió #2052
    Correccions:
    - Canviar PersonaInterceptor per recuperar les dades de l'usuari amb el
    plugin.

--- helium-back/src/main/java/es/caib/helium/back/interceptor/PersonaInterceptor.java
+++ helium-back/src/main/java/es/caib/helium/back/interceptor/PersonaInterceptor.java
@@ -4,12 +4,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import es.caib.helium.commons.config.BaseConfig;
 import es.caib.helium.commons.dto.PersonaDto;
+import es.caib.helium.logic.intf.service.AplicacioService;
 
 /**
  * Interceptor per guardar a la sessió les dades de la persona
@@ -19,6 +21,9 @@ import es.caib.helium.commons.dto.PersonaDto;
 @Component
 public class PersonaInterceptor implements HandlerInterceptor {
 	public static final String VARIABLE_SESSIO_PERSONA = "dadesPersona";
+
+	@Autowired
+	private AplicacioService aplicacioService;
 
 	public boolean preHandle(
 			HttpServletRequest request,
@@ -30,19 +35,9 @@ public class PersonaInterceptor implements HandlerInterceptor {
 		if(persona != null) return true;
 		
 		if (request.getUserPrincipal() != null) {
-			if(request.getUserPrincipal() instanceof OAuth2AuthenticationToken) {
-				OAuth2AuthenticationToken principal = (OAuth2AuthenticationToken) request.getUserPrincipal();
-				principal.getAuthorities();
-				persona = new PersonaDto(
-						principal.getPrincipal().getAttribute("preferred_username"), // codi
-						principal.getPrincipal().getAttribute("given_name"), // nom
-						principal.getPrincipal().getAttribute("family_name"), // llinatges
-						principal.getPrincipal().getAttribute("email"), // email
-						null // sexe
-						);
-				persona.setAdmin(request.isUserInRole("ROLE_ADMIN") || request.isUserInRole("HEL_ADMIN"));
-				session.setAttribute(VARIABLE_SESSIO_PERSONA, persona);
-			}
+			persona = aplicacioService.findPersonaAmbCodi(request.getUserPrincipal().getName());
+			persona.setAdmin(request.isUserInRole(BaseConfig.ROLE_ADMIN));
+			session.setAttribute(VARIABLE_SESSIO_PERSONA, persona);
 		}
 		return true;
 	}
