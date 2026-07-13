7fafd2ef943ef9e737647d9be0204a8103be2286
Josep Gayà <josepg@limit.es>
2026-07-13 16:24:54 +0200

    #2071 Corregir la sincronització amb Rolsac2
    - Llevats camps innecesaris dels objectes request per a la API de
    Rolsac2

--- helium-plugin-impl/src/main/java/net/conselldemallorca/helium/integracio/plugins/procediment/Rolsac2ProcedimentFilterRequest.java
+++ helium-plugin-impl/src/main/java/net/conselldemallorca/helium/integracio/plugins/procediment/Rolsac2ProcedimentFilterRequest.java
@@ -8,74 +8,7 @@ import lombok.Setter;
 @Setter
 @Builder
 public class Rolsac2ProcedimentFilterRequest implements Cloneable {
-	private String codigoUA;
 	private String codigoUADir3;
-	private String codigoPublicoObjetivo;
-	// ids separados por comas
-	private String codigos;
-	// Compara con codigo, nombre, estado, tipo, codigoSia, estadoSia y codigoDir3Sia
-	private String textos;
-	private String codigoFormaInicio;
-	private String titulo;
-	private String codigoTipoProcedimiento;
-	private String codigoSilencioAdministrativo;
-	private String codigoFinVia;
-	private String codigo;
-	
-	/**
-	 * Possibles valors:
-	 * <ul>
-	 * <li>'D' = Definitivo</li>
-	 * <li>'M' = Modificado</li>
-	 * <li>'T' = Todos (publicado o modificado)</li>
-	 * <li>'A' = Ambos (publicado y modificado)</li>
-	 * </ul>
-	 */
-	private String estadoWF;
-	
-	/*
-	 * Possibles valors: 0/1
-	 * 1 = procedimientos comunes
-	 */
-	private Integer comun;
-	private String codigoSia;
-	private String codigoTram;
-	private String codigoPlantilla;
-	private String codigoPlataforma;
-	/**
-	 * Possibles valors:
-	 * <ul>
-	 * <li>'S'= Si</li>
-	 * <li>'N'= No</li>
-	 * </ul>
-	 */
-	private String tramiteVigente;
-	private String canalPresentacion;
-	
-	
-	/**
-	 * Possibles valors:
-	 * <ul>
-	 * <li>'PV'	= Pendent validació</li>
-	 * <li>'M'	= En modificació</li>
-	 * <li>'P'	='Publicat</li>
-	 * <li>'PT'	= Pendent tancar</li>
-	 * <li>'T'	= Tancat</li>
-	 * </ul>
-	 */
-	private String estado;
-	private Integer esPdu;
-	/**
-	 * Possibles valors:
-	 * <ul>
-	 * <li>'PV'	= Pendent validació</li>
-	 * <li>'M'	= En modificació</li>
-	 * <li>'P'	='Publicat</li>
-	 * <li>'PT'	= Pendent tancar</li>
-	 * <li>'T'	= Tancat</li>
-	 * </ul>
-	 */
-	private String estados;
 	/**
 	 * Possibles valors:
 	 * <ul>
@@ -94,49 +27,8 @@ public class Rolsac2ProcedimentFilterRequest implements Cloneable {
 	 * Possibles valors: 0/1
 	 */
 	private Integer buscarEnDescendientesUA;
-	/**
-	 * Data amb format "DD/MM/YYYY"
-	 */
-	private String fechaActualizacionSia;
-
-	/**
-	 * Data amb format "DD/MM/YYYY"
-	 */
-	private String fechaPublicacionDesde;
-	/**
-	 * Data amb format "DD/MM/YYYY"
-	 */
-	private String fechaPublicacionHasta;
-	/*
-	 * Possibles valors: 0/1
-	 * 1 = telematico
-	 */
-	private Integer telematico;
-	/**
-	 * Possibles valors:
-	 * <ul>
-	 * <li>0 = No habilitado</li>
-	 * <li>1 = Sí habilitado</li>
-	 * <li>null no filtra por campo</li>
-	 * <ul>
-	 */
-	private Integer disponibleFuncionarioHabilitado;
-	/**
-	 * Possibles valors:
-	 * <ul>
-	 * <li>0 = No habilitado</li>
-	 * <li>1 = Sí habilitado</li>
-	 * <li>null no filtra por campo</li>
-	 * <ul>
-	 */
-	private Integer disponibleApoderadoHabilitado;
-	private Long idEntidad;
 	private Rolsac2FiltrePaginacio filtroPaginacion;
 	private Rolsac2FiltreOrden orden;
-
-	private Object codigosNormativas;
-	private Object codigosPublicosObjetivos;
-	private Object codigosMaterias;
 
 	@Override
 	public Rolsac2ProcedimentFilterRequest clone() throws CloneNotSupportedException {
--- helium-plugin-impl/src/main/java/net/conselldemallorca/helium/integracio/plugins/procediment/Rolsac2ServicioFilterRequest.java
+++ helium-plugin-impl/src/main/java/net/conselldemallorca/helium/integracio/plugins/procediment/Rolsac2ServicioFilterRequest.java
@@ -1,7 +1,5 @@
 package net.conselldemallorca.helium.integracio.plugins.procediment;
 
-import java.util.List;
-
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,65 +8,7 @@ import lombok.Setter;
 @Setter
 @Builder
 public class Rolsac2ServicioFilterRequest implements Cloneable {
-	public Object plataforma;
-	public Object version;
-	public Object vigente;
-	public Object codigoMateria;
-	public Object codigoTramiteTelematico;
-	public Object parametros;
-	public Object versionTramiteTelematico;
-	public Object estadoUA;
-
-	private String codigoUA;
 	private String codigoUADir3;
-	// ids separados por comas
-	private String codigos;
-	// Compara con codigo, nombre, estado, tipo, codigoSia, estadoSia y
-	// codigoDir3Sia
-	private String textos;
-	private String titulo;
-	private String codigo;
-
-	/**
-	 * Possibles valors:
-	 * <ul>
-	 * <li>'D' = Definitivo</li>
-	 * <li>'M' = Modificado</li>
-	 * <li>'T' = Todos (publicado o modificado)</li>
-	 * <li>'A' = Ambos (publicado y modificado)</li>
-	 * </ul>
-	 */
-	private String estadoWF;
-
-	/*
-	 * Possibles valors: 0/1 1 = procedimientos comunes
-	 */
-	private Integer comun;
-	private String codigoSia;
-	private String codigoPlataforma;
-
-	/**
-	 * Possibles valors:
-	 * <ul>
-	 * <li>'PV' = Pendent validació</li>
-	 * <li>'M' = En modificació</li>
-	 * <li>'P' ='Publicat</li>
-	 * <li>'PT' = Pendent tancar</li>
-	 * <li>'T' = Tancat</li>
-	 * </ul>
-	 */
-	private String estado;
-	/**
-	 * Possibles valors:
-	 * <ul>
-	 * <li>'PV' = Pendent validació</li>
-	 * <li>'M' = En modificació</li>
-	 * <li>'P' ='Publicat</li>
-	 * <li>'PT' = Pendent tancar</li>
-	 * <li>'T' = Tancat</li>
-	 * </ul>
-	 */
-	private String estados;
 	/**
 	 * Possibles valors:
 	 * <ul>
@@ -86,21 +26,6 @@ public class Rolsac2ServicioFilterRequest implements Cloneable {
 	 * Possibles valors: 0/1
 	 */
 	private Integer buscarEnDescendientesUA;
-	/**
-	 * Data amb format "DD/MM/YYYY"
-	 */
-	private String fechaActualizacionSia;
-
-	/**
-	 * Data amb format "DD/MM/YYYY"
-	 */
-	private String fechaPublicacionDesde;
-	/**
-	 * Data amb format "DD/MM/YYYY"
-	 */
-	private String fechaPublicacionHasta;
-	private Long idEntidad;
-	
 	private Rolsac2FiltrePaginacio filtroPaginacion;
 	private Rolsac2FiltreOrden orden;
 	
