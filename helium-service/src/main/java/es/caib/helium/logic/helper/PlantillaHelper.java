af08c44ccaab1e26f8bf77939aa350d59e847b0e
Josep Gayà <josepg@limit.es>
2026-09-16 09:22:06 +0200

    #2081 Mantenir format de text en textArea en la generació de documents

--- helium-service/src/main/java/es/caib/helium/logic/helper/PlantillaHelper.java
+++ helium-service/src/main/java/es/caib/helium/logic/helper/PlantillaHelper.java
@@ -834,7 +834,11 @@ public class PlantillaHelper {
 		template.setContentWrapper(new DocumentTemplate.ContentWrapper() {
 			public String wrapContent(String content) {
 				return "[#ftl]\n"
-						+ "[#escape any as any?xml?replace(\"[\\n|\\r|\\r\\n|\\n\\r]\",\"</text:p> <text:p>\")]\n"
+						+ "[#escape any as any?xml"
+						+ "?replace(\"  \", \" <text:s/>\", \"r\")"
+						+ "?replace(\"\\n\\n\", \"</text:p><text:p>\", \"r\")"
+						+ "?replace(\"(\\r\\n|\\n\\r|\\r|\\n)\", \"<text:line-break/>\", \"r\")"
+						+ "]\n"
 						+ content
 						+ "[/#escape]";
 			}
